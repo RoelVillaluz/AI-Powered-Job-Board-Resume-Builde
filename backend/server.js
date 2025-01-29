b8714f22c50d2cc244fb1a75ee682cb3fa8a4864
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-29 14:14:46 +0800

    added job posting routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"; 
 import  userRoutes  from "./routes/userRoutes.js"
+import jobPostingRoutes from "./routes/jobPostingRoutes.js"
+
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json()); 
 
 app.use('/api/users', userRoutes)
+app.use('/api/job_postings', jobPostingRoutes)
 
 app.listen(5000, () => {
     connectDB();

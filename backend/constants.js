dcb344d168bb248fa8aa7756978da8443c3f4770
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-29 13:53:05 +0800

    updated send response and status messages to display model name in message

--- backend/constants.js
+++ backend/constants.js
@@ -1,21 +1,23 @@
 export const STATUS_MESSAGES = {
     SUCCESS: {
-        FETCH: { code: 200, message: "Data fetched successfully" },
-        CREATE: { code: 201, message: "Data created successfully" },
-        UPDATE: { code: 200, message: "Data updated successfully" },
-        DELETE: { code: 200, message: "Data deleted successfully" },
+        FETCH: { code: 200, message: "(model) fetched successfully" },
+        CREATE: { code: 201, message: "(model) created successfully" },
+        UPDATE: { code: 200, message: "(model) updated successfully" },
+        DELETE: { code: 200, message: "(model) deleted successfully" },
     },
     ERROR: {
         SERVER: { code: 500, message: "Server Error" },
-        NOT_FOUND: { code: 404, message: "Resource not found" },
+        NOT_FOUND: { code: 404, message: "(model) not found" },
         BAD_REQUEST: { code: 400, message: "Invalid request" },
         EMAIL_EXISTS: { code: 400, message: "Email is already being used" },
         MISSING_FIELD: (field) => ({ code: 400, message: `Please provide a ${field}` }),
     }
 };
 
-export const sendResponse = (res, { code, success = true, data = null, message }) => {
-    const response = { success, message };
+export const sendResponse = (res, { code, success = true, data = null, message }, model = '') => {
+    const formattedMessage = model ? message.replace('(model)', model) : message;
+    const response = { success, formattedMessage };
+    
     if (data) response.data = data;
 
     return res.status(code).json(response)

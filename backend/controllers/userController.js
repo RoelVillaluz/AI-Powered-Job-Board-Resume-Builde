c5290d65dac142dda21f6935dff13e4e278f8f79
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-29 13:02:10 +0800

    added updateUser and deleteUser api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,3 +53,32 @@ export const createUser = async (req, res) => {
         res.status(500).json({ success: false, message: 'Server error' });
     }
 }
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params
+    const user = req.body
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, user, { new: true })
+
+        if (!updatedUser) {
+            return res.status(404).json({ success: false, message: 'User not found' })
+        }
+        res.status(200).json({ success: true, data: updatedUser })
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params
+    const user = req.body
+    
+    try {
+        await User.findByIdAndDelete(id)
+        res.status(200).json({ success: true, message: 'User deleted successfully' })
+    } catch (error) {
+        console.log(error);
+    }
+}

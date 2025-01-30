723aae8a14a562ca0c2d64c703f6d7c90b2ca97f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-30 20:30:50 +0800

    added hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { DataProvider } from './DataProvider'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from './Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar/>
         <DataProvider>
             <Routes>
               <Route path={"/"} element={<Home/>}/> 
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,26 +1,51 @@
 import { useEffect } from "react";
 import { useData } from "./DataProvider.jsx"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Home () {
-    const { users, setUsers, getAllData } = useData();
+    const { users = [], jobPostings = [], resumes = [], getAllData } = useData();
 
     useEffect(() => {
         document.title = 'Home'
 
-        getAllData(["users"])
+        getAllData(["users", "job-postings", "resumes"])
     }, [])
 
     return (
         <>
-            <h1>Home</h1>
-            <ul>
-                {users.map((user) =>(
-                    <li key={user._id}>
-                        <h3>{user.username}</h3>
-                        
-                    </li>
-                ))}
-            </ul>
+            <main className="hero-section">
+                {/* <header>
+                    <h1>Your Future Starts Here</h1>
+                    <p>Whether you're a fresh graduate, a seasoned professional, 
+                        or looking for a career switch, explore thousands of job openings 
+                        tailored to your expertise.</p>
+                        <Link className="call-to-action-link">
+                            Get Started
+                            <FontAwesomeIcon icon={faArrowRight} className="cta-icon" />
+                        </Link>
+                </header>
+                <figure>
+                    <img src="public/media/pexels-tima-miroshnichenko-6694958.jpg" alt="" />
+                </figure> */}
+               <figure>
+                    <img src="/public/media/pexels-fauxels-3182812.jpg" alt="" />
+               </figure>
+               <header>
+                    <h1>Ready to Take the Next Step?</h1>
+                    <div className="right">
+                        <p>Whether you're a fresh graduate, a seasoned professional, 
+                            or looking for a career switch, explore thousands of job openings 
+                            tailored to your expertise.
+                        </p>
+                        <Link className="call-to-action-link">
+                            Get Started
+                            <FontAwesomeIcon icon={faArrowRight} className="cta-icon" />
+                        </Link>
+                    </div>
+               </header>
+            </main>
         </>
     )
 }

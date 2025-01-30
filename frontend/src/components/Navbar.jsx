5200d41daf5689941b2e5060852baebea0998982
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-31 07:58:51 +0800

    added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 
 function Navbar () {
-
+    return (
+        <>
+            <nav className="navbar">
+                <Link className="logo" path={'/'}>
+                    <img src="/public/media/icons8-opportunity-24.png" alt="" />
+                </Link>
+                <ul className="navbar-links">
+                    <li><Link path={''}>Jobs</Link></li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar

c9af1f16cc414d73086832d536751c123fab89f5
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-31 07:59:03 +0800

    removed uneccessary comment

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -16,19 +16,6 @@ function Home () {
     return (
         <>
             <main className="hero-section">
-                {/* <header>
-                    <h1>Your Future Starts Here</h1>
-                    <p>Whether you're a fresh graduate, a seasoned professional, 
-                        or looking for a career switch, explore thousands of job openings 
-                        tailored to your expertise.</p>
-                        <Link className="call-to-action-link">
-                            Get Started
-                            <FontAwesomeIcon icon={faArrowRight} className="cta-icon" />
-                        </Link>
-                </header>
-                <figure>
-                    <img src="public/media/pexels-tima-miroshnichenko-6694958.jpg" alt="" />
-                </figure> */}
                <figure>
                     <img src="/public/media/pexels-fauxels-3182812.jpg" alt="" />
                </figure>

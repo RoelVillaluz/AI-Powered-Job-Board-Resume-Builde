d4cb023eba5635b2c1e7ad9a72a3cea862e34104
RoelVillaluz <roelo.villaluz@gmail.com>
2025-01-31 09:49:41 +0800

    added community section for hero section option 1

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -20,16 +20,28 @@ function Home () {
                     <img src="/public/media/pexels-fauxels-3182812.jpg" alt="" />
                </figure>
                <header>
-                    <h1>Ready to Take the Next Step?</h1>
+                    <div className="left">
+                        <h1>Ready to Take the Next Step?</h1>
+                        <Link className="call-to-action-link">
+                            Get Started
+                            <FontAwesomeIcon icon={faArrowRight} className="cta-icon" />
+                        </Link>
+                    </div>
                     <div className="right">
                         <p>Whether you're a fresh graduate, a seasoned professional, 
                             or looking for a career switch, explore thousands of job openings 
                             tailored to your expertise.
                         </p>
-                        <Link className="call-to-action-link">
-                            Get Started
-                            <FontAwesomeIcon icon={faArrowRight} className="cta-icon" />
-                        </Link>
+                        <div className="community">
+                            <div className="people-images">
+                                <img src="/public/media/pexels-kampus-6605421.jpg" alt="" />
+                                <img src="/public/media/pexels-anthonyshkraba-production-8278885.jpg" alt="" />
+                                <img src="/public/media/pexels-alipli-15003448.jpg" alt="" />
+                                <img src="/public/media/pexels-moose-photos-170195-1587009.jpg" alt="" />
+                                <span className="count">100+</span>
+                            </div>
+                            <p>Join our fast-growing community.</p>
+                        </div>
                     </div>
                </header>
             </main>

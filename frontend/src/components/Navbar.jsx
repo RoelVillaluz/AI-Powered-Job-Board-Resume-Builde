4b389c3dc910920493b5a3bfd995b18734757a16
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-01 18:24:26 +0800

    Conditionally hide navbar links on register and login pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar () {
+    const location = useLocation();
+    const hideLocation = location.pathname == '/register'
     return (
         <>
             <nav className="navbar">
-                <Link className="logo" path={'/'}>
+                <Link className="logo" to={'/'}>
                     <img src="/public/media/icons8-opportunity-24.png" alt="" />
                 </Link>
-                <ul className="navbar-links">
-                    <li><Link to='/register' className="sign-in-link">Sign In</Link></li>
-                </ul>
+                {!hideLocation && (
+                    <ul className="navbar-links">
+                        <li><Link to='/register' className="sign-in-link">Sign In</Link></li>
+                    </ul>
+                )}
             </nav>
         </>
     )

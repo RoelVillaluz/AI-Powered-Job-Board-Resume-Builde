2c392ddfa5dbac1a15ff28451380bb47781240c7
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-04 09:43:37 +0800

    added steps section to 'get-started' page

--- frontend/src/pages/MultiStepForm.jsx
+++ frontend/src/pages/MultiStepForm.jsx
@@ -8,8 +8,54 @@ function MultiStepForm() {
 
     return (
         <>
-            <div className="form-container">
+            <div className="form-container" id="get-started-form">
                 <div className="steps">
+                    <header>
+                        <h2>Let's get started</h2>
+                        <p className="subheader">
+                            You've successfully verified your email. 
+                            Let's set up your profile to get the best experience.
+                        </p>
+                    </header>
+                    <ul>
+                        <li className="active">
+                            <i class="fa-solid fa-user"></i>
+                            <div>
+                                <span>Choose your role.</span>
+                                <p className="supporting-text">Pick job seeker or employer to customize your experience.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-info"></i>
+                            <div>
+                                <span>Add details.</span>
+                                <p className="supporting-text">Fill in your info to generate your resume.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-lightbulb"></i>
+                            <div>
+                                <span>Skills</span>
+                                <p className="supporting-text">Add skills to boost your resume and get job matches.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-file-invoice"></i>
+                            <div>
+                                <span>Pick a resume template</span>
+                                <p className="supporting-text">Choose a template, and we'll populate it for you.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-door-open"></i>
+                            <div>
+                                <span>Welcome!</span>
+                                <p className="supporting-text">You're ready! Start your journey.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="right">
                     
                 </div>
             </div>

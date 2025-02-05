1e5f9d94d6864c59ecb4c39baa5809d85e68ab12
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-05 19:20:08 +0800

    added 'selected-role' section
    - selected role section for choosing between jobseeker or employer role

--- frontend/src/pages/MultiStepForm.jsx
+++ frontend/src/pages/MultiStepForm.jsx
@@ -88,14 +88,32 @@ function MultiStepForm() {
                 </div>
                 <div className="form-panel">
                     {currentStepIndex === 0 && (
-                        <div id="select-role">
-                            <h2>Choose your role</h2>
-                            <div className="row">
-                                <div className="option">
-                                    <i class="fa-solid fa-magnifying-glass"></i>
+                        <section className="select-role">
+                            <header>
+                                <h3>Let's start with your role!</h3>    
+                                <p>Tell us what position you're looking for.</p>
+                            </header>
+                            <div className="role-choices">
+                                <div className={`role-choice ${selectedRole === 'jobseeker' ? 'selected': ''}`}
+                                    onClick={() => setSelectedRole("jobseeker")}>
+                                        <i class="fa-solid fa-magnifying-glass"></i>
+                                        <label htmlFor="role-radio-btn">Jobseeker</label>
+                                        <input type="radio" name="role" id="role-radio-btn"/>
+                                        <div className="checked-indicator">
+                                            <i class="fa-solid fa-check"></i>
+                                        </div>
+                                </div>
+                                <div className={`role-choice ${selectedRole === 'employer' ? 'selected' : ''}`}
+                                    onClick={() => setSelectedRole("employer")}>
+                                        <i class="fa-solid fa-building"></i>
+                                        <label htmlFor="role-radio-btn">Employer</label>
+                                        <input type="radio" name="role" id="role-radio-btn"/>
+                                        <div className="checked-indicator">
+                                            <i class="fa-solid fa-check"></i>
+                                        </div>
                                 </div>
                             </div>
-                        </div>
+                        </section>
                     )}
                     {currentStepIndex === 1 && (
                         <div id="details">

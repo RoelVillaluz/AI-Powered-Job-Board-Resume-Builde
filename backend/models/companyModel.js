409b78d2a6e76ee069c1593cf23c8f125d076261
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-13 07:53:08 +0800

    created companyModel

---
+++ backend/models/companyModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        requried: true
+    },
+    industry: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: String
+    },
+    size: {
+        type: Number,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: String
+    },
+    jobs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "JobPosting"
+    }]
+}, { timestamps: true })
+
+
+export default mongoose.model("Company", companySchema);

fb9afd75d422ec91c4ec0030677a8280a264c1a0
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-16 15:33:30 +0800

    added WelcomeSection

---
+++ frontend/src/components/MultiStepForm/WelcomeSection.jsx
@@ -0,0 +1,17 @@
+function WelcomeSection({ selectedRole }) {
+    return (
+        <section className="welcome-section">
+            <header>
+                <h3>Welcome aboard!</h3>
+                <p>
+                    {selectedRole === 'jobseeker'
+                        ? 'You’re all set! Your profile is ready, and employers can now discover your skills and experience'
+                        : 'Great job! Your company profile is set up, and you’re now ready to connect with top talent.'
+                    }
+                </p>
+            </header>
+        </section>
+    )
+}
+
+export default WelcomeSection

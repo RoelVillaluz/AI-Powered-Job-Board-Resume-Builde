6b0c77706ba1f898802ffef6b692a2bab5fe1d03
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-21 15:15:11 +0800

    refactor: moved animations to 'animations.css'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/styles.css'
 import './styles/navbar.css'
 import './styles/forms.css'
+import './styles/animations.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(

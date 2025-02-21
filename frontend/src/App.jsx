987b43f3aab73b42cd5b37131e68f1084abceedf
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-21 14:54:30 +0800

    added useAuth to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,31 @@
-import { DataProvider, useData } from './DataProvider';
+import { DataProvider } from "./DataProvider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import Navbar from './components/Navbar';
-import Register from './pages/Register';
-import MultiStepForm from './pages/MultiStepForm';
-import Dashboard from './pages/Dashboard';
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import MultiStepForm from "./pages/MultiStepForm";
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider, useAuth } from "./components/AuthProvider";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <DataProvider>
-        <Routes>
-          <Route path={"/"} element={<AppRoutes />}/>
-          <Route path={"/register"} element={<Register />} />
-          <Route path={"/get-started"} element={<MultiStepForm />} />
-        </Routes>
-      </DataProvider>
+      <AuthProvider>
+        <Navbar />
+        <DataProvider>
+          <Routes>
+            <Route path="/" element={<AppRoutes />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/get-started" element={<MultiStepForm />} />
+          </Routes>
+        </DataProvider>
+      </AuthProvider>
     </Router>
   );
 }
 
 function AppRoutes() {
-  const { user } = useData(); // Destructure here inside the DataProvider context
+  const { user } = useAuth(); // Access user state from AuthProvider
 
   return user ? <Dashboard /> : <LandingPage />;
 }

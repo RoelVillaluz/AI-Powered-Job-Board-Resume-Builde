0997ccc74e022738f155d302ab75dd62364a2dea
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-22 08:30:05 +0800

    added 'sign-in' path to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 import MultiStepForm from "./pages/MultiStepForm";
 import Dashboard from "./pages/Dashboard";
 import { AuthProvider, useAuth } from "./components/AuthProvider";
@@ -15,7 +16,10 @@ function App() {
         <DataProvider>
           <Routes>
             <Route path="/" element={<AppRoutes />} />
+
             <Route path="/register" element={<Register />} />
+            <Route path="/sign-in" element={<SignIn />} />
+
             <Route path="/get-started" element={<MultiStepForm />} />
           </Routes>
         </DataProvider>

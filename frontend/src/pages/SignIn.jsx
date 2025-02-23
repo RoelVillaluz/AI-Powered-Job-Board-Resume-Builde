29c5abf73f97299d95457359223cd1be7b2acd05
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-23 12:57:07 +0800

    added login functionality to 'SignIn.jsx'

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useData } from "../DataProvider"
+import { useAuth } from "../components/AuthProvider"
+import axios from "axios"
 
 function SignIn() {
+    const [error, setError] = useState('')
     const [formData, setFormData] = useState({
-        name: '',
+        email: '',
         password: ''
     })
+    const { baseUrl } = useData();
+    const { login } = useAuth();
+
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value })
@@ -15,13 +23,30 @@ function SignIn() {
         document.title = 'Sign In'
     }, [])
 
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        setError('')
+        try {
+            // Login user
+            const response = await axios.post(`${baseUrl}/users/login`, formData)
+            // extract user and token
+            const { user, token } = response.data.data
+
+            login(user, token)
+            navigate('/');
+        } catch (error) {
+            console.error('Error', error)
+            setError(error)
+        }
+    }
+
     return (
         <>
             <div className="form-container" id="authentication-form-container">
                 <figure className="authentication-form-image-container">
                     <img src="public/media/pexels-olly-927022.jpg" alt="" />
                 </figure>
-                <form id="sign-in-form">
+                <form id="sign-in-form" onSubmit={handleFormSubmit}>
                     <header>
                         <h1>Welcome back!</h1>
                     </header>
@@ -38,7 +63,7 @@ function SignIn() {
                     </div>
 
                     <div className="form-group">
-                        <input type="text" onChange={handleChange} name="name" value={formData.name} placeholder="Enter your Username" />
+                        <input type="text" onChange={handleChange} name="email" value={formData.email} placeholder="Enter your email" />
                     </div>
                     <div className="form-group">
                         <input type="password" onChange={handleChange} name="password" value={formData.password} placeholder="Enter your password" />

da1f00e1a2dbcd7f9fb8ad3e7d356c48e82432e8
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-25 13:41:31 +0800

    added 'my-jobs' section to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import Layout from "../components/Layout"
+import { Link } from "react-router-dom"
 
 function Dashboard () {
 
@@ -10,7 +11,66 @@ function Dashboard () {
     return (
         <>
             <Layout>
-                
+                <main className="dashboard">
+                    <section className="grid-item" id="my-jobs">
+                        <header>
+                            <h3>My Jobs</h3>
+                            <Link>
+                                <i className="fa-solid fa-angle-right"></i>
+                            </Link>
+                        </header>
+                        <ul>
+                            <Link>
+                                <li>
+                                    <div className="row">
+                                        <i className="fa-solid fa-bookmark"></i>
+                                        <div>
+                                            <strong>Saved</strong>
+                                            <p>(0) jobs</p>
+                                        </div>
+                                    </div>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </li>
+                            </Link>
+                            <Link>
+                                <li>
+                                    <div className="row">
+                                        <i className="fa-solid fa-file-invoice"></i>
+                                        <div>
+                                            <strong>Applied</strong>
+                                            <p>(0) jobs</p>
+                                        </div>
+                                    </div>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </li>
+                            </Link>
+                            <Link>
+                                <li>
+                                    <div className="row">
+                                        <i className="fa-solid fa-clipboard-question"></i>
+                                        <div>
+                                            <strong>Interviews</strong>
+                                            <p>(0) jobs</p>
+                                        </div>
+                                    </div>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </li>
+                            </Link>
+                            <Link>
+                                <li>
+                                    <div className="row">
+                                        <i className="fa-solid fa-briefcase"></i>
+                                        <div>
+                                            <strong>Offers</strong>
+                                            <p>(0) jobs</p>
+                                        </div>
+                                    </div>
+                                    <i className="fa-solid fa-angle-right"></i>
+                                </li>
+                            </Link>
+                        </ul>
+                    </section>  
+                </main>
             </Layout>
         </>
     )

53f9f5e9b293a8c5ce86812683193175c191b465
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-25 17:05:15 +0800

    added getResumesByUser to routes

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createResume, deleteResume, getResume, getResumes, updateResume } from "../controllers/resumeController.js"
+import { createResume, deleteResume, getResume, getResumes, getResumesByUser, updateResume } from "../controllers/resumeController.js"
 
 const router = express.Router()
 
 router.get('/', getResumes)
 router.get('/:id', getResume)
+router.get('/user/:userId', getResumesByUser)
 
 router.post('/', createResume)
 router.patch('/:id', updateResume)

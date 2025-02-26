00f5128adc1b3b55a2a2ffbac0d9ea9d371f612f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-02-26 14:03:58 +0800

    refactored side navbar links to be more dynamic
    - added links array containing "to" and "icon" for each list item

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { useAuth } from "./AuthProvider";
 function Navbar () {
     const location = useLocation();
     const navigate = useNavigate();
-    const hideLocation = location.pathname == '/register' || location.pathname == '/login' || location.pathname == '/get-started' || location.pathname == '/sign-in'
-    const { user, setUser } = useAuth();
+    const hideNavbarLocations = ['/register', '/login', '/get-started', '/sign-in']
+    const hideLocation = hideNavbarLocations.some(path => location.pathname === path)
     const [isVisible, setIsVisible] = useState(false);
 
     const handleLogout = () => {
@@ -15,82 +15,38 @@ function Navbar () {
         navigate('')
     }
 
+    const links = [
+        { to: '/', icon: 'fa-solid fa-house'},
+        { to: '/jobs', icon: 'fa-solid fa-briefcase'},
+        { to: '/messages', icon: 'fa-solid fa-envelope'},
+        { to: '/resumes', icon: 'fa-solid fa-file-invoice'},
+    ]
+
     return (
         <>
-            {/* <nav className='navbar'>
-                <Link className="logo" to={'/'}>
-                    <img src="/public/media/icons8-opportunity-24.png" alt="" />
-                </Link>
-                {!hideLocation && (
-                    <ul className="navbar-links">
-                        {user ? (
-                            <li className="nav-icon" onClick={() => setIsVisible(!isVisible)}>
-                                <i className="fa-regular fa-user"></i>
-                                <ul className={`dropdown ${isVisible ? 'visible': 'hidden'}`}>
-                                    <li>
-                                        <Link>
-                                            <i className="fa-solid fa-user"></i>
-                                            <span>My Profile</span>
+            {!hideLocation && (
+                <aside className="sidebar-navbar">
+                    <nav>
+                        <Link className="logo" to={'/'}>
+                            <img src="/public/media/icons8-opportunity-24.png" alt="" />
+                        </Link>
+                        <ul>
+                        <ul>
+                            {links.map(({ to, icon }) => (
+                                    <li key={to} className={location.pathname === to ? "active" : ""}>
+                                        <Link to={to}>
+                                            <i className={icon}></i>
                                         </Link>
                                     </li>
-                                    {user.role === 'employer' && (
-                                        <li>
-                                            <Link to={'/create-job-posting'}>
-                                                <i className="fa-solid fa-briefcase"></i>
-                                                <span>Create Job</span>
-                                            </Link>
-                                        </li>
-                                    )}
-                                    <li onClick={handleLogout}>
-                                        <Link>
-                                            <i className="fa-solid fa-right-from-bracket"></i>
-                                            <span>Sign Out</span>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-                        ) : (
-                            <li>
-                                <Link to="/sign-in" className="sign-in-link">
-                                    Sign In
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
-                )}
-            </nav> */}
-            <aside className="sidebar-navbar">
-                <nav>
-                    <Link className="logo" to={'/'}>
-                        <img src="/public/media/icons8-opportunity-24.png" alt="" />
-                    </Link>
-                    <ul>
-                        <li>
-                            <Link to={'/'}>
-                                <i className="fa-solid fa-house"></i>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/jobs'}>
-                                <i className="fa-solid fa-briefcase"></i>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/messages'}>
-                                <i className="fa-solid fa-envelope"></i>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/resumes'}>
-                                <i className="fa-solid fa-file-invoice"></i>
-                            </Link>
-                        </li>
-                    </ul>
-                    <Link to={'/settings'}>
-                        <i class="fa-solid fa-gear"></i>
-                    </Link>
-                </nav>
-            </aside>
+                                ))}
+                            </ul>
+                        </ul>
+                        <Link to={'/settings'}>
+                            <i className="fa-solid fa-gear"></i>
+                        </Link>
+                    </nav>
+                </aside>
+            )}
         </>
     )
 }

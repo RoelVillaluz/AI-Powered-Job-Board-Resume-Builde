af6c516afe1084b0f3244a7a1c4a136cb3986e36
RoelVillaluz <roelo.villaluz@gmail.com>
2025-03-10 10:02:02 +0800

    removed date variables from 'ApplicationProgressSection'

--- frontend/src/components/Dashboard/ApplicationProgressSection.jsx
+++ frontend/src/components/Dashboard/ApplicationProgressSection.jsx
@@ -1,23 +1,6 @@
 import { Link } from "react-router-dom";
 
 function ApplicationProgressSection() {
-    const currentDate = new Date();
-    const date = currentDate.getDate();
-    const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(currentDate);
-    const futureDate = new Date();
-
-    const getFutureDates = () => {
-        const dates = []
-
-        for (let i = 0; i <= 6; i++) {
-            futureDate.setDate(date + i);
-            const dayNumber = futureDate.getDate();
-            const dayName = futureDate.toLocaleDateString("en-US", {weekday: "short"})
-            dates.push({ number: dayNumber, day: dayName })
-        }
-        return dates
-    }
-
     return(
         <>
             <section className="grid-item" id="application-progress">

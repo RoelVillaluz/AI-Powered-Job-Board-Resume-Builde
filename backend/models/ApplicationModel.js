2d9822a5dc3895a5e692283331f75a7b702bf2e6
RoelVillaluz <roelo.villaluz@gmail.com>
2025-03-12 08:03:49 +0800

    created ApplicationModel

---
+++ backend/models/ApplicationModel.js
@@ -0,0 +1,45 @@
+import mongoose, { mongo } from "mongoose";
+
+const applicationSchema = new mongoose.Schema({
+    jobPosting: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "JobPosting",
+        required: true,
+    },
+    applicant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    resume: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Resume',
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Reviewed', 'Accepted', 'Rejected'],
+        default: 'Pending'
+    },
+    questions: [{
+        question: {
+            type: String,
+        },
+        answer: {
+            type: String
+        }
+    }],
+    appliedAt: {
+        type: Date,
+        default: Date.now
+    },
+    reviewedAt: {
+        type: Date,
+        default: null
+    },
+})
+
+applicationSchema.pre('save')
+
+const Application = new mongoose.model("Application", applicationSchema)
+export default Application

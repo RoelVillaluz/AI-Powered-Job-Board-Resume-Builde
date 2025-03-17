28a3773dabb05e330e12e81f9e0440714f1da0ee
RoelVillaluz <roelo.villaluz@gmail.com>
2025-03-17 17:09:09 +0800

    updated #profile section in dashboard

---
+++ frontend/src/pages/ProfileDetailPage.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { useData } from "../DataProvider";
+import { useParams } from "react-router-dom";
+
+function ProfileDetailPage() {
+    const [profile, setProfile] = useState(null);
+    const { userId } = useParams();
+    const { baseUrl } = useData();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(`${baseUrl}/users/${userId}`)
+                console.log(response.data.data)
+                setProfile(response.data.data)
+            } catch (error) {
+                console.log('Error', error)
+            }
+        }
+        fetchProfile()
+    }, [userId])
+
+    useEffect(() => {
+        document.title = `${profile.name}'s Profile`
+    })
+
+    return (
+        <>
+            <header>
+                <div className="banner"></div>
+            </header>
+        </>
+    )
+}
+
+export default ProfileDetailPage

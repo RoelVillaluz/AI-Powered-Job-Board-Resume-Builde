c48e82151dd1e0cfd003ece2d7505bc900fd8295
RoelVillaluz <roelo.villaluz@gmail.com>
2025-03-19 12:38:18 +0800

    added messages map for rating and message based on resume rating

--- frontend/src/components/Dashboard/ProfileScoreSection.jsx
+++ frontend/src/components/Dashboard/ProfileScoreSection.jsx
@@ -10,6 +10,29 @@ function ProfileScoreSection() {
 
     const rotation = `rotate(${progress / 2}turn)`;
 
+    const messages = {
+        0.25: {
+          rating: "Poor",
+          message: "Your resume needs significant improvement. Consider adding more details about your experience and skills.",
+        },
+        0.5: {
+          rating: "Average",
+          message: "Your resume is decent, but there’s room for improvement. Try refining your descriptions and adding measurable achievements.",
+        },
+        0.75: {
+          rating: "Good",
+          message: "Your resume is well-structured! A few tweaks and refinements could make it even stronger.",
+        },
+        0.9: {
+            rating: "Great",
+            message: "You're almost there! Your resume stands out, but adding more tailored keywords and formatting improvements could take it to the next level.",
+        },
+        1: {
+          rating: "Excellent",
+          message: "Nearly flawless! Your resume effectively presents your qualifications",
+        },
+    };
+
     const showMessage = (value) => {
         if (value <= 0.25) return 'Your resume is bad';
         if (value > 0.25 && value <= 0.50) return 'Your resume is average';

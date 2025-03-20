07255d02edd2e9d50d13e53b3c321d027ab730fd
RoelVillaluz <roelo.villaluz@gmail.com>
2025-03-20 20:47:36 +0800

    changed "get_user_resumes" into "get_resumes_by_id"

--- backend/python_scripts/utils.py
+++ backend/python_scripts/utils.py
@@ -1,7 +1,10 @@
+import datetime
 import os
 from dotenv import load_dotenv
+import numpy as np
 import pymongo
 from bson import ObjectId
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 load_dotenv()
 
@@ -9,8 +12,15 @@ mongo_uri = os.getenv('MONGO_URI')
 client = pymongo.MongoClient(mongo_uri)
 db = client["database"]
 
-def get_user_resumes(user_id):
-    """ Fetches all resumes for a specific user from the MongoDB database. """
+def get_resumes_by_id(resume_id):
+    try:
+        resume_data = db.resumes.find_one({"_id": ObjectId(resume_id)})
+        return resume_data
+    except Exception as e:
+        print(f"Error fetching resume for {resume_id}: {e}")
+        return None
+
+
     if not user_id:
         return []
     

7652f7e6cf7bfdc99384c929a048e7c3d9f7bb4a
RoelVillaluz <roelo.villaluz@gmail.com>
2025-04-03 09:00:36 +0800

    created component: "JobPostingCard' for reusability

---
+++ frontend/src/components/JobPostingCard.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom"
+
+const JobPostingCard = ({ job }) => {
+    
+    return (
+        <li key={job._id}>
+            <Link to={`job-postings/${job._id}`}>
+                <div className="wrapper">
+                    <img src={job.company.logo} alt="" />
+                    <div>
+                        <h2>{job.title}</h2>
+                        <h3>{job.company.name}</h3>
+                    </div>
+                </div>
+                <h4>${job.salary}</h4>
+                <p>{job.summary}</p>
+
+                <div className="details">
+                    <div className="tags-list">
+                        <div className="tag-item">
+                            <i className="fa-solid fa-location-dot" aria-hidden="true"></i>
+                            <span>{job.location}</span>
+                        </div>
+                        <div className="tag-item">
+                            <i className="fa-solid fa-briefcase" aria-hidden="true"></i>
+                            <span>{job.jobType}</span>
+                        </div>
+                        <div className="tag-item">
+                            <i className="fa-solid fa-user-tie" aria-hidden="true"></i>
+                            <span>{job.experienceLevel}</span>
+                        </div>
+                    </div>
+                    
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, veniam labore! Aspernatur culpa alias illum autem ipsa harum laudantium adipisci omnis minus quibusdam similique, eveniet accusantium excepturi aliquam a praesentium!</p>
+
+                    <div className="actions">
+
+                        <button id="apply-btn">Apply</button>
+                        <button id="save-btn">
+                            <i className='fa-solid fa-bookmark'></i>
+                        </button>
+                        <div className="match-score">
+                            {job.similarity}% Match
+                        </div>
+                    </div>
+                </div>
+
+            </Link>
+        </li>
+    )
+}
+
+export default JobPostingCard

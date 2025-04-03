ab536c8318337f8b0f73a7d46f775c94b0746d6c
RoelVillaluz <roelo.villaluz@gmail.com>
2025-04-03 09:01:06 +0800

    refactored jobsPostingsList to use component: "JobPostingCard" for easier readability

--- frontend/src/pages/JobPostingsList.jsx
+++ frontend/src/pages/JobPostingsList.jsx
@@ -4,6 +4,7 @@ import { useData } from "../DataProvider";
 import axios from "axios";
 import Layout from "../components/Layout";
 import { Link } from "react-router-dom";
+import JobPostingCard from "../components/JobPostingCard";
 
 function JobPostingsList() {
     const { user } = useAuth();
@@ -65,50 +66,7 @@ function JobPostingsList() {
                             </header>
                             <ul>
                                 {jobRecommendations.map((job) => (
-                                    <li key={job._id}>
-                                        <Link to={`job-postings/${job._id}`}>
-                                            <div className="wrapper">
-                                                <img src={job.company.logo} alt="" />
-                                                <div>
-                                                    <h2>{job.title}</h2>
-                                                    <h3>{job.company.name}</h3>
-                                                </div>
-                                            </div>
-                                            <h4>${job.salary}</h4>
-                                            <p>{job.summary}</p>
-
-                                            <div className="details">
-                                                <div className="tags-list">
-                                                    <div className="tag-item">
-                                                        <i className="fa-solid fa-location-dot" aria-hidden="true"></i>
-                                                        <span>{job.location}</span>
-                                                    </div>
-                                                    <div className="tag-item">
-                                                        <i className="fa-solid fa-briefcase" aria-hidden="true"></i>
-                                                        <span>{job.jobType}</span>
-                                                    </div>
-                                                    <div className="tag-item">
-                                                        <i className="fa-solid fa-user-tie" aria-hidden="true"></i>
-                                                        <span>{job.experienceLevel}</span>
-                                                    </div>
-                                                </div>
-                                                
-                                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, veniam labore! Aspernatur culpa alias illum autem ipsa harum laudantium adipisci omnis minus quibusdam similique, eveniet accusantium excepturi aliquam a praesentium!</p>
-
-                                                <div className="actions">
-
-                                                    <button id="apply-btn">Apply</button>
-                                                    <button id="save-btn">
-                                                        <i className='fa-solid fa-bookmark'></i>
-                                                    </button>
-                                                    <div className="match-score">
-                                                        {job.similarity}% Match
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                        </Link>
-                                    </li>
+                                    <JobPostingCard job={job}/>
                                 ))}
                             </ul>
                         </section>

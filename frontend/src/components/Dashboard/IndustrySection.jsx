2e1047e5cb208527ea779505c62d87642175b5a3
RoelVillaluz <roelo.villaluz@gmail.com>
2025-04-15 13:34:04 +0800

    added header to IndustrySection

---
+++ frontend/src/components/Dashboard/IndustrySection.jsx
@@ -0,0 +1,13 @@
+function IndustrySection () {
+    return (
+        <section className="company-industry">
+            <header>
+                <h3>Select Your Industry</h3>
+                <p>Please choose the industry that best represents your company.</p>
+            </header>
+            
+        </section>
+    )
+}
+
+export default IndustrySection

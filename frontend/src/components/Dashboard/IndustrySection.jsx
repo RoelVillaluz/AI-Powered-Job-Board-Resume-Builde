acb03c6995e56a2785bf93f7dd7ac4f9ffd89869
RoelVillaluz <roelo.villaluz@gmail.com>
2025-04-16 10:15:44 +0800

    updated addOrRemoveIndustry to also update formdata.industry for form submission

--- frontend/src/components/Dashboard/IndustrySection.jsx
+++ frontend/src/components/Dashboard/IndustrySection.jsx
@@ -1,7 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { industryChoices } from "../../../../backend/constants"
 
-function IndustrySection () {
+function IndustrySection ({ formData, handleChange }) {
+    const [selectedIndustries, setSelectedIndustries] = useState(formData.industry || [])
+
+    const addOrRemoveIndustry = (industry) => {
+        
+        const updatedIndustries = selectedIndustries.includes(industry) 
+            ? selectedIndustries.filter((i) => i !== industry)
+            : [...selectedIndustries, industry]
+
+        setSelectedIndustries(updatedIndustries)
+
+        handleChange({
+            target: {
+                name: "industry",
+                value: updatedIndustries
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (formData.industry && Array.isArray(formData.industry)) {
+            setSelectedIndustries(formData.industry)
+        }
+    }, [formData.industry])
+
+    useEffect(() => {
+        console.log('Form Data Industries: ', formData.industry)
+    }, [formData.industry])
+
     return (
         <section className="company-industry">
             <header>

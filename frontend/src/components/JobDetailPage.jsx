153e1a3f934d32e0779b33493134c23d69c42022
RoelVillaluz <roelo.villaluz@gmail.com>
2025-04-22 09:31:35 +0800

    added icons to job details page for job type, experience level, salary, applicants

--- frontend/src/components/JobDetailPage.jsx
+++ frontend/src/components/JobDetailPage.jsx
@@ -36,6 +36,38 @@ function JobDetailPage() {
                                 <h3>{job?.company?.name} • {job?.location}</h3>
                             </div>
                         </header>
+                        <ul id="icons"> 
+                            <li>
+                                <i className="fa-regular fa-clock" aria-hidden="true"></i>
+                                <div>
+                                    <span>Job Type</span>
+                                    <h4>{job?.jobType}</h4>
+                                </div>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-user-tie" aria-hidden="true"></i>
+                                <div>
+                                    <span>Experience Level</span>
+                                    <h4>{job?.experienceLevel}</h4>
+                                </div>
+                            </li>
+                            <li>
+                                <i className="fa-regular fa-money-bill-1" aria-hidden="true"></i>
+                                <div>
+                                    <span>Salary</span>
+                                    <h4>${Number(job?.salary).toLocaleString()} / year</h4> 
+                                </div>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-users" aria-hidden="true"></i>
+                                <div>
+                                    <span>Number of applicants</span>
+                                    <h4>0</h4> 
+                                </div>
+                            </li>
+                        </ul>
+
+                    </section>
                     </section>
 
                 </main> 

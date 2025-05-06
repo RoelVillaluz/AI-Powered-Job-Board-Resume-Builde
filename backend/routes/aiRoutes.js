a75d1660f5f66a76d53ef048c28a9e424c6af8d4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-06 16:42:10 +0800

    added route for compare resume and job function

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getJobRecommendations, getRecommendedSkills, getResumeScore, getPredictedSalary, recommendCompanies } from "../controllers/aiController.js";
+import { getJobRecommendations, getRecommendedSkills, getResumeScore, getPredictedSalary, recommendCompanies, compareResumeAndJob } from "../controllers/aiController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/skill-recommendations/:userId', getRecommendedSkills)
 router.get('/resume-score/:resumeId', getResumeScore)
 router.get('/predicted-salary/:resumeId', getPredictedSalary)
 router.get('/recommend-companies/:userId', recommendCompanies)
+router.get('/compare/:resumeId/:jobId', compareResumeAndJob)
 
 export default router

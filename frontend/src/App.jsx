6a0b3605218aedaef48489f18ac9bc7caf58d178
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-11 17:27:46 +0800

    added function scrollToTop to fix issue where redirecting to new page sometimes rendered the bottom of the page once redirected as starting point

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { DataProvider } from "./DataProvider";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SideNavbar from "./components/SideNavbar";
 import Register from "./pages/Register";
@@ -10,6 +10,17 @@ import { AuthProvider, useAuth } from "./components/AuthProvider";
 import CreateJobPosting from "./pages/CreateJob";
 import JobPostingsList from "./pages/JobPostingsList";
 import JobDetailPage from "./components/JobDetailPage";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
@@ -17,25 +28,26 @@ function App() {
       <AuthProvider>
         <SideNavbar />
         <DataProvider>
-          <Routes>
-            
-            <Route path="/" element={<AppRoutes />} />
 
+          {/* Scroll to top on every route change */}
+          <ScrollToTop />
+
+          <Routes>
+            <Route path="/" element={<AppRoutes />} />
             <Route path="/register" element={<Register />} />
             <Route path="/sign-in" element={<SignIn />} />
-
             <Route path="/get-started" element={<MultiStepForm />} />
-
-            <Route path="/create-job-posting" element={<CreateJobPosting/>} />
-            <Route path="/job-postings" element={<JobPostingsList/>} />
+            <Route path="/create-job-posting" element={<CreateJobPosting />} />
+            <Route path="/job-postings" element={<JobPostingsList />} />
             <Route path="/job-postings/:jobId" element={<JobDetailPage />} />
-
           </Routes>
+          
         </DataProvider>
       </AuthProvider>
     </Router>
   );
 }
+
 
 function AppRoutes() {
   const { user } = useAuth(); // Access user state from AuthProvider

ccec99b11e5cf75ab97bb285a13c79c97a101322
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-14 08:16:00 +0800

    changed name to createJobForm

--- frontend/src/pages/CreateJobForm.jsx
+++ frontend/src/pages/CreateJobForm.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useAuth } from "../components/AuthProvider.jsx";
 import Layout from "../components/Layout.jsx";
 
-function CreateJobPosting() {
+function CreateJobForm() {
     const { baseUrl } = useData();
     const { user } = useAuth();
     const [formData, setFormData] = useState({
@@ -114,71 +114,13 @@ function CreateJobPosting() {
                         </ul>
 
                     </div>
-                    <form className="form-panel">
+                    <form className="form-panel" style={{ flex: '4' }}>
+                        
                     </form>
                 </div>
-                {/* <h1>Create Job Posting</h1>
-                <form action="" onSubmit={handleFormSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="">Job Title</label>
-                        <input name="title" value={formData.title} type="text" onChange={handleChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Location</label>
-                        <input name="location" value={formData.location} type="text" onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Job Type</label>
-                        <input name="jobType" value={formData.jobType} type="text" onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Experience Level</label>
-                        <input name="experienceLevel" value={formData.experienceLevel} type="text" onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Salary</label>
-                        <input name="salary" value={formData.salary} type="number" onChange={handleChange} />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Requirements</label>
-                        <input
-                            name="requirements"
-                            value={requirementInput}
-                            type="text"
-                            onChange={(e) => setRequirementInput(e.target.value)} 
-                            onKeyDown={handleAddItem} 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Skills</label>
-                        <input
-                            name="skills"
-                            value={skillInput}
-                            type="text"
-                            onChange={(e) => setSkillInput(e.target.value)} 
-                            onKeyDown={handleAddItem} 
-                        />
-                    </div>
-                    <button>Submit</button>
-                </form>
-
-                <h3>Requirements</h3>
-                <ul>
-                    {formData.requirements.map((skill, index) => (
-                        <li key={index}>{skill}</li>
-                    ))}
-                </ul>
-
-                <h3>Skills</h3>
-                <ul>
-                    {formData.skills.map((skill, index) => (
-                        <li key={index}>{skill.name}</li>
-                    ))}
-                </ul> */}
             </Layout>
         </>
     )
 }
 
-export default CreateJobPosting
+export default CreateJobForm

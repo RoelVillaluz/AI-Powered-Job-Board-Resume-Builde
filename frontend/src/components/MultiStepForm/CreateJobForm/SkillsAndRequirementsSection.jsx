35b850e7298b2a864377c8ccbb4494aa5788b5d8
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-20 09:15:27 +0800

    refactor: extract DynamicInputWithDropdown component from SkillsAndRequirementsSection

--- frontend/src/components/MultiStepForm/CreateJobForm/SkillsAndRequirementsSection.jsx
+++ frontend/src/components/MultiStepForm/CreateJobForm/SkillsAndRequirementsSection.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import React from 'react';
+
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import DynamicInputWithDropdown from "../../FormComponents/DynamicInputWithDropdown";
 
 function SkillsAndRequirementsSection({ formData, setFormData, handleChange }) {
     const formTypes = {
@@ -102,51 +105,26 @@ function SkillsAndRequirementsSection({ formData, setFormData, handleChange }) {
             <div className="form-details">
                 
                 {Object.entries(formTypes).map(([name, config]) => (
-                    <>
-                        <div className="form-group" key={name}>
-                            <label>{config.label}</label>
-                            
-                            <div className="row" style={{ alignItems: 'start' }}>
-
-                                <input 
-                                    type="text" 
-                                    value={inputs[name] || ""}
-                                    onChange={(e) => handleInputChange(e, name)}
-                                    onKeyDown={(e) => handleAddItem(e, name)}
-                                />
-
-                                {config.hasDropDown && (
-                                    <ul className="select-menu">
-                                        <button onClick={() => toggleVisibility(config.dropDownName)} className="toggle-dropdown-btn" type="button">
-                                            {inputs[config.dropDownField] || config.options[0]}
-                                            <i className="fa-solid fa-angle-down"></i>
-                                        </button>
-                                        <ul className={`dropdown-list ${isVisible[formTypes[name].dropDownName] ? 'visible' : ''}`}>
-                                            {getFilteredOptions(config.options, inputs[config.dropDownField]).map((option, index) => (
-                                                <li 
-                                                    key={index}
-                                                    onClick={() => { 
-                                                        setInputs(prev => ({ ...prev, level: option }))
-                                                        toggleVisibility("skillDropdown")
-                                                    }}
-                                                >
-                                                <span className="option-text">{option}</span>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </ul>
-                                )}
-
-                            </div>
-
-                        </div>
+                    <React.Fragment key={name}>
+                        <DynamicInputWithDropdown
+                            key={name}
+                            config={config}
+                            name={name}
+                            inputs={inputs}
+                            handleInputChange={handleInputChange}
+                            handleAddItem={handleAddItem}
+                            setInputs={setInputs}
+                            isVisible={isVisible}
+                            toggleVisibility={toggleVisibility}
+                            getFilteredOptions={getFilteredOptions}
+                        />
 
                         {/* <DragDropContext onDragEnd={handleDragEnd}>
                             <Droppable droppableId="">
 
                             </Droppable>
                         </DragDropContext> */}
-                    </>
+                    </React.Fragment>
                 ))}
 
             </div>

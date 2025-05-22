76771ac651a320af90036a85d2c89e94627474e3
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-23 07:32:00 +0800

    updated added items list to show "+ number" of more items after 3 items have been added already

--- frontend/src/components/DraggableList.jsx
+++ frontend/src/components/DraggableList.jsx
@@ -11,20 +11,33 @@ function DraggableList({ listKey, items, handleDragEnd, handleRemoveListItem }) 
                         ref={provided.innerRef}
                     >
                         {items?.length > 0 ? (
-                            items.map((item, index) => (
+                        <>
+                            {items.slice(0, 3).map((item, index) => (
                                 <Draggable key={item.name || item} draggableId={item.name || item} index={index}>
                                     {(provided) => (
-                                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className='draggable'>
-                                            <span>
-                                                {item.name || item}
-                                                {/* For skills with level */}
-                                                {item.level && item.level !== 'Level' ? ` - ${item.level}` : ''}
-                                            </span>
-                                            <i className='fa-solid fa-xmark' onClick={() => handleRemoveListItem(listKey, index)}></i>
-                                        </li>
+                                    <li 
+                                        ref={provided.innerRef} 
+                                        {...provided.draggableProps} 
+                                        {...provided.dragHandleProps} 
+                                        className='draggable'
+                                    >
+                                        <span>
+                                        {item.name || item}
+                                        {item.level && item.level !== 'Level' ? ` - ${item.level}` : ''}
+                                        </span>
+                                        <i 
+                                        className='fa-solid fa-xmark' 
+                                        onClick={() => handleRemoveListItem(listKey, index)}
+                                        ></i>
+                                    </li>
                                     )}
                                 </Draggable>
-                            ))
+                                ))}
+
+                                {items.length > 3 && (
+                                <li className='more-items'>+{items.length - 3}</li>
+                            )}
+                        </>
                         ) : (
                             <p>No {listKey} added yet.</p>
                         )}

1cd77f403b353985185ff548f2d3ae8d81e34e8f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-24 06:34:27 +0800

    changed imports for draggableList

--- frontend/src/components/DraggableList.jsx
+++ frontend/src/components/DraggableList.jsx
@@ -1,4 +1,4 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
 
 function DraggableList({ listKey, items, handleDragEnd, handleRemoveListItem }) {
     return (

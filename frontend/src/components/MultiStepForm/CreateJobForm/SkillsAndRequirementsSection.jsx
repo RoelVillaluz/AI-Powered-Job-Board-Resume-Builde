d27534b2b693583b685c3ffda01a2071eb12ba91
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-24 06:34:48 +0800

    removed unused imports from skillsAndReq

--- frontend/src/components/MultiStepForm/CreateJobForm/SkillsAndRequirementsSection.jsx
+++ frontend/src/components/MultiStepForm/CreateJobForm/SkillsAndRequirementsSection.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import React from 'react';
 
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import DynamicInputWithDropdown from "../../FormComponents/DynamicInputWithDropdown";
 import DraggableList from "../../DraggableList";
 

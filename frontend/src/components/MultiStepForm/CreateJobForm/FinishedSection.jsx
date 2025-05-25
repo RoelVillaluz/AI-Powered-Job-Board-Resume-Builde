322ba6cf16f1b39667a75af3bc52d41afdc14604
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-25 14:59:33 +0800

    added header and icon to finishedSection

--- frontend/src/components/MultiStepForm/CreateJobForm/FinishedSection.jsx
+++ frontend/src/components/MultiStepForm/CreateJobForm/FinishedSection.jsx
@@ -1,6 +1,12 @@
 function FinishedSection() {
     return (
-        <></>
+       <section id="finished">
+            <header>
+                <h3>Ready to Post Your Job?</h3>
+                <p>Just hit submit to create the job.</p>
+            </header>
+            <i className="fa-solid fa-file-circle-check" id="finished-form-icon"></i>
+        </section>
     )
 }
 

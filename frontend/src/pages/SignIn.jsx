165b6d9f7abfd476d5b2958be42bf4145f46b56d
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-28 15:38:35 +0800

    changed "forgot password" into button

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -71,6 +71,7 @@ function SignIn() {
 
                     <button type="submit">Sign In</button>
                     <span id="sign-in-link-span">Don't have an account yet? <Link to={'/register'}>Create an account now!</Link></span>
+                    <button type="button" className="forgot-password-btn">Forgot Password?</button>
 
                 </form>
             </div>

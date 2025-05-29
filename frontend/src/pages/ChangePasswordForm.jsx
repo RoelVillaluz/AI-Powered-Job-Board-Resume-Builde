925162458702c52d45f6bc8547096db019dd3fd7
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-30 07:24:00 +0800

    added formData to changePasswordForm

--- frontend/src/pages/ChangePasswordForm.jsx
+++ frontend/src/pages/ChangePasswordForm.jsx
@@ -1,4 +1,11 @@
-function ChangePasswordForm() {
+import { useEffect, useState } from "react"
+import { useData } from "../DataProvider"
+    const { baseUrl } = useData();
+    const [formData, setFormData] = useState({
+        newPassword: '',
+        confirmNewPassword: ''
+    })
+
     return (
         <h1>Change password</h1>
     )

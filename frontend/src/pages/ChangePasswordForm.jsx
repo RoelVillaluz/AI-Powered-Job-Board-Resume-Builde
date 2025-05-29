3482c2a73b6c22768888e3624346b1a6c2234784
RoelVillaluz <roelo.villaluz@gmail.com>
2025-05-30 07:24:44 +0800

    added handleChange and form inputs for changePasswordForm

--- frontend/src/pages/ChangePasswordForm.jsx
+++ frontend/src/pages/ChangePasswordForm.jsx
@@ -5,9 +5,30 @@ import { useData } from "../DataProvider"
         newPassword: '',
         confirmNewPassword: ''
     })
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
 
     return (
-        <h1>Change password</h1>
+        <div className="form-container" id="authentication-form-container">
+
+            
+
+            <form action="" id="change-password-form">
+                <header>
+                    <h1>Change your password</h1>
+                </header>
+
+                <div className="form-group">
+                    <input type="text" name='formData.newPassword' placeholder="New password" onChange={(e) => handleChange(e)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" name='formData.confirmNewPassword' placeholder="Confirm new password" onChange={(e) => handleChange(e)}/>
+                </div>
+
+            </form>
+
+        </div>
     )
 }
 

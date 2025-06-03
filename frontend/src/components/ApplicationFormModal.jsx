94590b721232db4e5fe81c50ea936494891f49f0
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-04 07:55:57 +0800

    added image to application form modal

--- frontend/src/components/ApplicationFormModal.jsx
+++ frontend/src/components/ApplicationFormModal.jsx
@@ -3,14 +3,18 @@ function ApplicationFormModal({ job }) {
     return (
         <div className="blurry-overlay">
             <div className="application-form-container">
+
                 <div className="image-container">
                     <img src="/media/pexels-cytonn-955389.jpg" alt="" />
                 </div>
+
                 <form action="" className="application-form-modal">
+                    
                     <header>
                         <h1>Apply for this role</h1>
                         <h3>{job?.title} - {job?.company.name}</h3>
                     </header>
+
                     {job?.preScreeningQuestions.map((question, index) => (
                         <div className="form-group">
                             <label htmlFor="" key={index}>{question.question}</label>
@@ -18,6 +22,7 @@ function ApplicationFormModal({ job }) {
                         </div>
                     ))}
                 </form>
+
             </div>
         </div>
     )

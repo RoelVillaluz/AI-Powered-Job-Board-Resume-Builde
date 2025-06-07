c1664a9fd92db0588485d4a9b67dc69260cce77f
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-07 09:16:48 +0800

    created messageModel

---
+++ backend/models/messageModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sentAt: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true })
+
+const Message = mongoose.model('Message', messageSchema)
+export default Message

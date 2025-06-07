c057c895cf70da93df1059bf0879c87cadb43e39
RoelVillaluz <roelo.villaluz@gmail.com>
2025-06-07 08:02:55 +0800

    created functions getApplication, getApplicationById, getApplicationsByUser functions

---
+++ backend/controllers/applicationController.js
@@ -0,0 +1,39 @@
+import Application from "../models/applicationModel.js";
+import { STATUS_MESSAGES, sendResponse } from '../constants.js';
+
+export const getApplications = async (req, res) => {
+    try {
+        const applications = await Application.find({})
+
+        return sendResponse(res, { ...STATUS_MESSAGES.SUCCESS.FETCH, data: applications }, 'Applications');
+    } catch (error) {
+        console.error('Error fetching applications: ', error)
+        return sendResponse(res, { ...STATUS_MESSAGES.ERROR.SERVER, success: false });
+    }
+}
+
+export const getApplicationById = async (req, res) => {
+    const { applicationId } = req.params;
+
+    try {
+        const application = await Application.findById(applicationId)
+
+        return sendResponse(res, { ...STATUS_MESSAGES.SUCCESS.FETCH, data: application }, 'Application');
+    } catch (error) {
+        console.error('Error fetching application: ', error)
+        return sendResponse(res, { ...STATUS_MESSAGES.ERROR.SERVER, success: false });
+    }
+}
+
+export const getApplicationsByUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const application = await Application.findById(applicationId)
+
+        return sendResponse(res, { ...STATUS_MESSAGES.SUCCESS.FETCH, data: application }, 'Application');
+    } catch (error) {
+        console.error('Error fetching application: ', error)
+        return sendResponse(res, { ...STATUS_MESSAGES.ERROR.SERVER, success: false });
+    }
+}

3b513f6abcdce4d3e7b25335f00e387a6e8efaa1
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-01 13:56:58 +0800

    updated messages with updatedAt to show time of last edit

--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -530,34 +530,38 @@ function ChatsPage() {
                                             <time datetime={group.rawDateTime}>{group.createdAt}</time>
                                             <div className="messages">
                                                 {group.messages.map((message) => (
-                                                    <div className="message-bubble" 
-                                                        key={message._id} 
-                                                        onClick={() => {
-                                                            if (selectedMessage?._id === message._id) {
-                                                                setSelectedMessage(null)
-                                                            } else {
-                                                                setSelectedMessage(message)
-                                                            }
-                                                        }}
-                                                    >
-                                                        <span>{message.content}</span>
-                                                        <div className={`${selectedMessage?._id === message._id ? 'actions visible': 'actions'}`}>
-                                                            {message.sender._id === user._id ? (
-                                                                <>
-                                                                <button id="edit-message-btn" onClick={(e) => handleMessageButtonAction(e, "edit", message)}>
-                                                                    <i className="fa-solid fa-pen-to-square"></i>
-                                                                </button>
-                                                                <button className="negative" id="delete-message-btn" onClick={(e) => handleMessageButtonAction(e, "delete", message)}>
-                                                                    <i className="fa-solid fa-trash"></i>
-                                                                </button>
-                                                                </>
-                                                            ) : (
-                                                                <button className="negative" id="report-message-btn">
-                                                                    <i className="fa-solid fa-bullhorn"></i>
-                                                                </button>
-                                                            )}
+                                                    <React.Fragment key={message._id}>
+                                                        {message.updatedAt !== null && (
+                                                            <time dateTime={message.updatedAt}>Edited on {formatDate(message.updatedAt)}</time>
+                                                        )}
+                                                        <div className="message-bubble"
+                                                            onClick={() => {
+                                                                if (selectedMessage?._id === message._id) {
+                                                                    setSelectedMessage(null)
+                                                                } else {
+                                                                    setSelectedMessage(message)
+                                                                }
+                                                            }}
+                                                        >
+                                                            <span>{message.content}</span>
+                                                            <div className={`${selectedMessage?._id === message._id ? 'actions visible': 'actions'}`}>
+                                                                {message.sender._id === user._id ? (
+                                                                    <>
+                                                                    <button id="edit-message-btn" onClick={(e) => handleMessageButtonAction(e, "edit", message)}>
+                                                                        <i className="fa-solid fa-pen-to-square"></i>
+                                                                    </button>
+                                                                    <button className="negative" id="delete-message-btn" onClick={(e) => handleMessageButtonAction(e, "delete", message)}>
+                                                                        <i className="fa-solid fa-trash"></i>
+                                                                    </button>
+                                                                    </>
+                                                                ) : (
+                                                                    <button className="negative" id="report-message-btn">
+                                                                        <i className="fa-solid fa-bullhorn"></i>
+                                                                    </button>
+                                                                )}
+                                                            </div>
                                                         </div>
-                                                    </div>
+                                                    </React.Fragment>
                                                 ))}
                                             </div>
                                         </div>

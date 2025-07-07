d8942c3da3f7f5daf924d5d79f9bd12c043a03a4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-07 09:28:19 +0800

    memoized MessageGroup and added functions: "containerClass" and "renderedMessages"

--- frontend/src/components/Chat/MessageGroup.jsx
+++ frontend/src/components/Chat/MessageGroup.jsx
@@ -1,24 +1,42 @@
+import { useAuth } from "../../contexts/AuthProvider";
+import { useChatContext } from "../../contexts/ChatContext"
 import MessageBubble from "./MessageBubble"
+import { React, memo, useMemo, useCallback } from "react";
 
-function MessageGroup({ group, message, selectedMessage, setSelectedMessage, user }) {
+const MessageGroup = memo(({ group, message }) => {
+    const { selectedMessage, setSelectedMessage } = useChatContext();
+    const { user } = useAuth();
+
+    // Memoize the class calculation
+    const containerClass = useMemo(() => {
+        return group.sender === user.name ? 'receiver' : ''
+    }, [group.sender, user.name])
+
+    // Memoize the message rendering
+    const renderedMessages = useMemo(() => {
+        return group.messages.map((message) => (
+            <MessageBubble 
+                key={message._id}
+                message={message}
+                selectedMessage={selectedMessage}
+                setSelectedMessage={setSelectedMessage}
+                user={user}
+            />
+        ));
+    }, [group.messages, selectedMessage, setSelectedMessage, user]);
+
+
     return (
-        <li className={group.sender === user.name ? 'receiver' : ''}>
+        <li className={containerClass}>
             <img src={`/${group.profilePicture}`} alt={`${group.sender}'s profile picture`} />
             <div className="message-group">
                 <time dateTime={group.rawDateTime}>{group.createdAt}</time>
                 <div className="messages">
-                    {group.messages.map((message) => (
-                        <MessageBubble 
-                            message={message}
-                            selectedMessage={selectedMessage}
-                            setSelectedMessage={setSelectedMessage}
-                            user={user}
-                        />  
-                    ))}
+                    {renderedMessages}
                 </div>
             </div>
         </li>
     )
-}
+})
 
 export default MessageGroup

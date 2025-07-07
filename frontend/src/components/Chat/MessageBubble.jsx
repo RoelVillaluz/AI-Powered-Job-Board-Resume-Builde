3996cab03c252fb7660f4b578cfa73d34d5cb4d1
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-07 09:29:42 +0800

    memoized values: isSelected, isOwnMessage, actionsClass, formattedEditTime for messageBubble

--- frontend/src/components/Chat/MessageBubble.jsx
+++ frontend/src/components/Chat/MessageBubble.jsx
@@ -1,14 +1,41 @@
-import React from "react"
+import React, { memo, useMemo, useCallback } from "react"
 import { useChatContext } from "../../contexts/ChatContext"
 import { formatDate } from "../utils/dateUtils";
 const MessageBubble = (({ message ,selectedMessage, setSelectedMessage, user }) => {
 
-    const { handleMessageButtonAction } = useChatContext();
+
+    // Memoize computed values
+    const isSelected = useMemo(() => {
+        return selectedMessage?._id === message._id
+    }, [selectedMessage?._id, message._id])
+
+    const isOwnMessage = useMemo(() => {
+        return message.sender._id === user._id
+    }, [message.sender._id, user._id])
+
+    const actionsClass = useMemo(() => {
+        return isSelected ? 'actions visible' : 'actions'
+    }, [isSelected])
+
+    const formattedEditTime = useMemo(() => {
+        return message.updatedAt ? formatDate(message.updatedAt) : null
+    }, [message.updatedAt])
+
+    // Memoize click handler
+    const handleMessageClick = useCallback(() => {
+        if (isSelected) {
+            setSelectedMessage(null)
+        } else {
+            setSelectedMessage(message)
+        }
+    }, [isSelected, selectedMessage, message])
 
     return (
         <React.Fragment key={message._id}>
-            {message.updatedAt !== null && (
-                <time dateTime={message.updatedAt}>Edited on {formatDate(message.updatedAt)}</time>
+            {formattedEditTime && (
+                <time dateTime={message.updatedAt}>
+                    Edited on {formattedEditTime}
+                </time>
             )}
             <div className="message-bubble"
                 onClick={() => {

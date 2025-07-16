f1469ebcf64a70786ae564d78fa9df02946a4bb6
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-16 16:48:32 +0800

    updated messageGroup to show 'seenAt' date if own message group and is seen by receiver

--- frontend/src/components/Chat/MessageGroup.jsx
+++ frontend/src/components/Chat/MessageGroup.jsx
@@ -1,8 +1,9 @@
 import { useAuth } from "../../contexts/AuthProvider";
 import MessageBubble from "./MessageBubble"
 import { markMessagesAsSeen } from "../../services/messageServices";
-import { React, memo, useMemo, useCallback, useRef } from "react";
+import { memo, useMemo, useCallback, useRef } from "react";
 import { useData } from "../../contexts/DataProvider";
+import { formatDate } from "../utils/dateUtils";
 
 const MessageGroup = memo(({ group }) => {
     const { user } = useAuth();
@@ -23,12 +24,30 @@ const MessageGroup = memo(({ group }) => {
         ));
     }, [messages, user])
 
+    const isOwnMessages = useMemo(() => {
+        if (!group.messages || group.messages.length === 0) return false;
+
+        const senderId = group.messages[0].sender._id
+
+        return senderId === user._id
+    }, [group.messages, user])
+
+    const isSeen = useMemo(() => {
+        if (!group.messages || group.messages.length === 0) return false;
+
+        return group.messages[0].seen
+    }, [group.messages])
 
     return (
         <li className={containerClass}>
             <img src={`/${profilePicture}`} alt={`${sender}'s profile picture`} />
             <div className="message-group">
-                <time dateTime={rawDateTime}>{createdAt}</time>
+                <div className="wrapper" style={{ gap: '0rem' }}>
+                    <time dateTime={rawDateTime}>{createdAt}</time>
+                    {isOwnMessages && isSeen && (
+                        <time>| Seen at {formatDate(group.messages[0].seenAt)}</time>
+                    )}
+                </div>
                 <div className="messages">
                     {renderedMessages}
                 </div>

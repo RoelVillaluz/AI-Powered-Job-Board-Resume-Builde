bbe22676471b6edcaedda46575b8f20e08981aeb
RoelVillaluz <roelo.villaluz@gmail.com>
2025-07-21 08:30:03 +0800

    refactored job dropdown logic  into useJobDropdown hook

--- frontend/src/components/JobListComponents/JobPostingsListSection.jsx
+++ frontend/src/components/JobListComponents/JobPostingsListSection.jsx
@@ -1,13 +1,17 @@
-import { useState, useMemo, useRef, useEffect } from "react";
+import { useState, useMemo, useRef, useEffect, useCallback } from "react";
 import { useJobFilters } from "../../contexts/JobsListContext";
 import JobPostingCard from "../JobPostingCard";
 import { useAuth } from "../../contexts/AuthProvider";
+import { useJobSortDropdown } from "../../hooks/jobs/useJobSortDropdown";
 import { useJobSorting } from "../../hooks/jobs/useJobSorting";
 
 function JobPostingsListSection() {
     const { user } = useAuth();
     const { filteredJobs } = useJobFilters();
 
+    const sortButtonClickedRef = useRef(false); 
+
+    const { dropdownRef, isDropdownVisible, setIsDropdownVisible, toggleDropdown } = useJobSortDropdown(sortButtonClickedRef);
     const { sortedJobs, sortTypes, handleSortButtonClick, currentSortType } = useJobSorting(filteredJobs, setIsDropdownVisible, sortButtonClickedRef);
 
     return (
@@ -16,10 +20,7 @@ function JobPostingsListSection() {
                 <h2>Recommended jobs <span className="filtered-jobs-count">{filteredJobs.length}</span></h2>
                 <div className="sorter" ref={dropdownRef}>
                     <h4>Sort by: <span className="sort-type">{currentSortType}</span></h4>
-                    <button className="sort-toggle" onClick={(e) => {
-                        e.stopPropagation();  
-                        setIsDropdownVisible(prev => !prev);
-                    }} aria-label="Toggle sort options">
+                    <button className="sort-toggle" onClick={() => toggleDropdown()} aria-label="Toggle sort options">
                         <i className="fa-solid fa-sort"></i>
                     </button>
                     <ul style={{ display: isDropdownVisible ? 'flex': 'none' }}>
@@ -35,12 +36,12 @@ function JobPostingsListSection() {
                 </div>
             </header>
             <ul>
-                {getSortedJobs.map((job) => (
+                {sortedJobs.map((job) => (
                     <JobPostingCard job={job} user={user} key={job._id}/>
                 ))}
             </ul>
         </section>
-    )
+    );
 }
 
 export default JobPostingsListSection

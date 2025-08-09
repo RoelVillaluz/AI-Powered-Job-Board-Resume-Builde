bc8163a518ef90608ced85164435da1fb1422d0e
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-09 08:37:40 +0800

    created useJobDetails hook

---
+++ frontend/src/hooks/jobs/useJobDetails.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export const useJobDetails = (baseUrl, jobId, user) => {
+    const [job, setJob] = useState(null);
+    const [company, setCompany] = useState(null);
+    const hasQuestions = job?.preScreeningQuestions?.length > 0
+
+    const [loading, setLoading] = useState(true);
+    const [isComparing, setIsComparing] = useState(false);
+
+    const [userPreferences, setUserPreferences] = useState({
+    
+    })
+    
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const response = await axios.get(`${baseUrl}/job-postings/${jobId}`)
+                console.log('Job Posting: ', response.data.data)
+                
+                setJob(response.data.data)
+            } catch (error) {
+                console.error('Error: ', error)
+            }
+        }
+        fetchJob()
+    }, [jobId])
+
+    useEffect(() => {
+        const fetchCompany = async () => {
+            if (!job?.company?._id) return; 
+
+            try {
+                const response = await axios.get(`${baseUrl}/companies/${job?.company?._id}`)
+                console.log('Company: ', response.data.data)
+
+                setCompany(response.data.data)
+            } catch (error) {
+                console.error('Error: ', error)
+            }
+        }
+        fetchCompany()
+    }, [job?.company?._id])
+
+    useEffect(() => {
+        if (job && company) {
+            setLoading(false)
+        }
+    }, [job, company])
+
+    
+
+}

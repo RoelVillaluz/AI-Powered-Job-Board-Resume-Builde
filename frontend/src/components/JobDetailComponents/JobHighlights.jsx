44270b31a6f1acb21337801500c866b7a6a238db
RoelVillaluz <roelo.villaluz@gmail.com>
2025-08-12 08:56:17 +0800

    separated JobHighlights into component

---
+++ frontend/src/components/JobDetailComponents/JobHighlights.jsx
@@ -0,0 +1,46 @@
+function JobHighlights({ job, company, loading }) {
+    return (
+        <section className="job-highlights">
+            <ul>
+                {!loading ? (
+                    <>
+                        <li>
+                            <i className="fa-solid fa-user-tie" aria-hidden="true"></i>
+                            <div>
+                                <h5>Experience Level</h5>
+                                <h3>{job.experienceLevel}</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <i className="fa-solid fa-briefcase" aria-hidden="true"></i>
+                            <div>
+                                <h5>Job Type</h5>
+                                <h3>{job.jobType}</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <i className="fa-solid fa-house-laptop" aria-hidden="true"></i>
+                            <div>
+                                <h5>Work Setup</h5>
+                                <h3>Remote</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <i className="fa-solid fa-industry" aria-hidden="true"></i>
+                            <div>
+                                <h5>Industry</h5>
+                                <h3>{company.industry[0]}</h3>
+                            </div>
+                        </li>
+                    </>
+                ) : (
+                    new Array(4).fill(null).map((_, index) => (
+                        <li className="skeleton" key={index}></li>
+                    ))                                   
+                )}
+            </ul>
+        </section>
+    )
+}
+
+export default JobHighlights
--- frontend/src/pages/JobDetailPage.jsx
+++ frontend/src/pages/JobDetailPage.jsx
@@ -13,6 +13,7 @@ import { useJobDetails } from "../hooks/jobs/useJobDetails";
 import { useResumeAnalysis } from "../hooks/resumes/useResumeAnalysis";
 import JobDetailHeader from "../components/JobDetailComponents/JobDetailHeader";
 import { useResume } from "../contexts/ResumesContext";
+import JobHighlights from "../components/JobDetailComponents/JobHighlights";
 
 function JobDetailPage() {
     const { baseUrl } = useData();
@@ -50,47 +51,8 @@ function JobDetailPage() {
                             hasQuestions={hasQuestions}
                             showModal={showModal}
                         />
-                        {/* <section className="job-highlights">
-                            <ul>
-                                {!loading ? (
-                                    <>
-                                        <li>
-                                            <i className="fa-solid fa-user-tie" aria-hidden="true"></i>
-                                            <div>
-                                                <h5>Experience Level</h5>
-                                                <h3>{job.experienceLevel}</h3>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <i className="fa-solid fa-briefcase" aria-hidden="true"></i>
-                                            <div>
-                                                <h5>Job Type</h5>
-                                                <h3>{job.jobType}</h3>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <i className="fa-solid fa-house-laptop" aria-hidden="true"></i>
-                                            <div>
-                                                <h5>Work Setup</h5>
-                                                <h3>Remote</h3>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <i className="fa-solid fa-industry" aria-hidden="true"></i>
-                                            <div>
-                                                <h5>Industry</h5>
-                                                <h3>{company.industry[0]}</h3>
-                                            </div>
-                                        </li>
-                                    </>
-                                ) : (
-                                    new Array(4).fill(null).map((_, index) => (
-                                        <li className="skeleton" key={index}></li>
-                                    ))                                   
-                                )}
-                            </ul>
-                        </section>
-
+                        <JobHighlights job={job} company={company} loading={loading}/>
+                        {/* 
                         <div className="wrapper">
 
                             <section id="job-description">

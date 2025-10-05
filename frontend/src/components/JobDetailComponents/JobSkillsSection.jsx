6e6e9f7ccbb690f6986d3f2de42aaf1ed185f9ce
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-05 11:50:59 +0800

    memoized checkedSkills in JobSkillsSection
    - fixed 0(n) lookup by checking if skills is inside memoized "checkedSkills" instead of repeatedly checking everytime for each skill

--- frontend/src/components/JobDetailComponents/JobSkillsSection.jsx
+++ frontend/src/components/JobDetailComponents/JobSkillsSection.jsx
@@ -1,8 +1,15 @@
 
+import { useMemo } from "react";
 import { useResumeActions } from "../../hooks/resumes/useResumeActions";
 
 function JobSkillsSection({ baseUrl, user, setUser, job, loading = false, currentResume, setCurrentResume }) {
     const { handleAddSkillToResume } = useResumeActions(baseUrl, user, setUser, setCurrentResume);
+
+    // Calculate once per render instead of per-checkbox
+    const checkedSkills = useMemo(() => 
+        new Set(currentResume?.skills.map(s => s._id) || []),
+        [currentResume?.skills]
+    )
 
     return (
         <section id="skills">
@@ -11,13 +18,13 @@ function JobSkillsSection({ baseUrl, user, setUser, job, loading = false, curren
                 <h3>Skills</h3>
                 {!loading ? (
                     <ul>
-                        {job.skills.map((skill, index) => (
-                            <li key={index}>
+                        {job.skills.map((skill) => (
+                            <li key={skill._id}>
                                 <div className="checkbox-wrapper-19">
                                     <input 
                                         type="checkbox" 
                                         id={`cbtest-19 ${skill.name}`} 
-                                        checked={currentResume?.skills.some(s => s.name === skill.name)}
+                                        checked={checkedSkills.has(skill._id)} // 0(1) lookup
                                         onChange={() => handleAddSkillToResume(currentResume?._id, skill)}
                                     />
                                     <label htmlFor={`cbtest-19 ${skill.name}`} className="check-box" />

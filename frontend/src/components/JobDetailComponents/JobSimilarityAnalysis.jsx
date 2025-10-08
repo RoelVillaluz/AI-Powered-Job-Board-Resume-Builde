1232888b143b13efaa861db9ebad91f13f710a7e
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-08 08:28:44 +0800

    refactor: changed "similarity-analysis" section into separate 'JobSimilarityAnalysis' file

---
+++ frontend/src/components/JobDetailComponents/JobSimilarityAnalysis.jsx
@@ -0,0 +1,47 @@
+import { useResume } from "../../contexts/ResumesContext";
+import { useResumeAnalysis } from "../../hooks/resumes/useResumeAnalysis";
+import Gauge from "../Gauge";
+
+function JobSimilarityAnalysis({ loading }) {
+    const { resumes, currentResume, setCurrentResume } = useResume();
+    const { resumeScore, isComparing, messages } = useResumeAnalysis();
+
+    return (
+        <section id="similarity-analysis">
+            {/* add gauge here later for similarity percentage */}
+            {/* add feature later here for resume selection */}
+            <section id="similarity-gauge">
+                <h3>Resume Analysis</h3>
+                <Gauge progress={resumeScore.totalScore} messages={messages} loading={isComparing} objectName={"Resume"}/>
+                
+                <div>
+                    <h6>Change Resume scorer later to make this have dynamic feedback</h6>
+                    <p>Strengths: High skill similarity</p>
+                    <p>Weaknesses: Low relevant work experience. No college degree yet.</p>
+                </div>
+                
+            </section>
+
+            <section id="resume-list">
+                <h3>Select Resume</h3>
+                <ol className="custom-ol">
+                    {resumes.map((resume, index) => (
+                        <li className={`custom-li ${currentResume._id === resume._id ? 'current': ''}`} key={resume._id}>
+                            <div className="wrapper">
+                                <h4>Resume {index + 1}</h4> 
+                                <i className="fa-solid fa-angle-down" aria-label="Toggle content visibility"></i>
+                            </div>
+                            {/* <span className="joined-skills">Matched skills: {getMatchedResumeSkills(resume)}</span> */}
+                        </li>
+                    ))}
+                </ol>
+                <button className="upload-resume-btn">
+                    <span>Or upload new resume</span>
+                    <i className="fa-solid fa-arrow-up"></i>
+                </button>
+            </section>
+        </section>
+    )
+}
+
+export default JobSimilarityAnalysis
--- frontend/src/pages/JobDetailPage.jsx
+++ frontend/src/pages/JobDetailPage.jsx
@@ -17,6 +17,7 @@ import JobHighlights from "../components/JobDetailComponents/JobHighlights";
 import JobDescription from "../components/JobDetailComponents/JobDescription";
 import JobSkillsSection from "../components/JobDetailComponents/JobSkillsSection";
 import JobCompany from "../components/JobDetailComponents/JobCompany";
+import JobSimilarityAnalysis from "../components/JobDetailComponents/JobSimilarityAnalysis";
 
 function JobDetailPage() {
     const { baseUrl } = useData();
@@ -45,6 +46,8 @@ function JobDetailPage() {
                 <main id="job-details-page">
 
                     <section id="job-details">
+
+                        {/* move props later directly into component since data is mutated */}
                         <JobDetailHeader
                             user={user}
                             job={job}
@@ -74,103 +77,11 @@ function JobDetailPage() {
                             company={company}
                             loading={loading}
                         />
-                        {/* 
-                        <section id="about-the-company">
-                            <h3>About the Company</h3>
-                            <div className="wrapper">
-                                <div id="company-details">
-                                    {!loading ? (
-                                        <p>{company?.description}</p>
-                                    ) : (
-                                        <div className="skeleton-text-group">
-                                            <div className="skeleton text max-width"></div>
-                                            <div className="skeleton text max-width"></div>
-                                            <div className="skeleton text max-width"></div>
-                                            <div className="skeleton text short"></div>
-                                        </div>
-                                    )}
-                                    <div className="row">
-                                        <div id="rating">
-                                            {!loading ? (
-                                                <img src={`/${company?.logo}`} alt={`${company?.name} logo`} />
-                                            ) : (
-                                                <div className="skeleton square"></div>
-                                            )}
-                                            <div style={{ marginTop: '8px' }}>
-                                                <h4>{!loading ? company?.name : 'Company Name'}</h4>
-                                                <span><i className="fa-solid fa-star"></i> {!loading ? company?.rating.toFixed(1) : '0.0'}</span>
-                                            </div>
-                                        </div>
-                                        {company?.ceo && (
-                                            <div id="ceo">
-                                                {!loading ? (
-                                                    company?.ceo.image ? (
-                                                        <img src={`/${company?.ceo?.image}`} alt={`${company?.name} CEO`} />
-                                                    ) : (
-                                                        <i className="fa-solid fa-user"></i>
-                                                    )
-                                                ) : (
-                                                    <div className="skeleton circle"></div>
-                                                )}
-                                                <div style={{ marginTop: '8px' }}>
-                                                    {!loading && (
-                                                        <h4>{company?.ceo?.name}</h4>
-                                                    )}
-                                                    <span>CEO</span>
-                                                </div>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                                {!loading ? (
-                                    <div className="images">
-                                        {company?.images.slice(0, 3).map((image, index) => (
-                                            <img src={`/${image}`} key={index}></img>
-                                        ))}
-                                    </div>
-                                ) : (
-                                    <div className="skeleton rectangle"></div>
-                                )}
-                            </div>
-                        </section>
 
                     </section>
-
-                    <section id="similarity-analysis">
-                        {/* add gauge here later for similarity percentage */}
-                        {/* add feature later here for resume selection */}
-                        {/* <section id="similarity-gauge">
-                            <h3>Resume Analysis</h3>
-                            <Gauge progress={resumeScore.totalScore} messages={messages} loading={isComparing} objectName={"Resume"}/>
-                            
-                            <div>
-                                <h6>Change Resume scorer later to make this have dynamic feedback</h6>
-                                <p>Strengths: High skill similarity</p>
-                                <p>Weaknesses: Low relevant work experience. No college degree yet.</p>
-                            </div>
-                            
-                        </section>
-
-                        <section id="resume-list">
-                            <h3>Select Resume</h3>
-                            <ol className="custom-ol">
-                                {resumes.map((resume, index) => (
-                                    <li className={`custom-li ${currentResume._id === resume._id ? 'current': ''}`} key={resume._id}>
-                                        <div className="wrapper">
-                                            <h4>Resume {index + 1}</h4> 
-                                            <i className="fa-solid fa-angle-down" aria-label="Toggle content visibility"></i>
-                                        </div>
-                                        <span className="joined-skills">Matched skills: {getMatchedResumeSkills(resume)}</span>
-                                    </li>
-                                ))}
-                            </ol>
-                            <button className="upload-resume-btn">
-                                <span>Or upload new resume</span>
-                                <i className="fa-solid fa-arrow-up"></i>
-                            </button>
-                        </section>  */}
-
-                    </section>
+                    <JobSimilarityAnalysis
+                        loading={loading}
+                    />
 
                 </main> 
             </Layout>

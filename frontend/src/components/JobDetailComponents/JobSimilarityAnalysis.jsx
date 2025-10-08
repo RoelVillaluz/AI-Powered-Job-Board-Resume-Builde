c47c52fc832d9a90369c004734f8087edc6e4bce
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-08 08:58:54 +0800

    refactored ResumeList into separate component within JobSimilarityAnalysis

--- frontend/src/components/JobDetailComponents/JobSimilarityAnalysis.jsx
+++ frontend/src/components/JobDetailComponents/JobSimilarityAnalysis.jsx
@@ -2,6 +2,40 @@ import { useResume } from "../../contexts/ResumesContext";
 import { useResumeAnalysis } from "../../hooks/resumes/useResumeAnalysis";
 import Gauge from "../Gauge";
 
+const ResumeList = ({ job, resumes, currentResume, setCurrentResume }) => {
+    const getMatchedResumeSkills = (resume) => {
+        if (!job?.skills || !resume?.skills) return '';
+
+        const jobSkills = job.skills.map(s => s.name.toLowerCase())
+        const matched = resume.skills.filter(skill => jobSkills.includes(skill.name.toLowerCase()))
+
+        return matched.length > 0 
+        ? matched.map(s => s.name).join(', ') 
+        : 'No matching skills';
+    }
+
+    return (
+        <section id="resume-list">
+            <h3>Select Resume</h3>
+            <ol className="custom-ol">
+                {resumes.map((resume, index) => (
+                    <li className={`custom-li ${currentResume._id === resume._id ? 'current': ''}`} key={resume._id}>
+                        <div className="wrapper">
+                            <h4>Resume {index + 1}</h4> 
+                            <i className="fa-solid fa-angle-down" aria-label="Toggle content visibility"></i>
+                        </div>
+                        <span className="joined-skills">Matched skills: {getMatchedResumeSkills(resume)}</span>
+                    </li>
+                ))}
+            </ol>
+            <button className="upload-resume-btn">
+                <span>Or upload new resume</span>
+                <i className="fa-solid fa-arrow-up"></i>
+            </button>
+        </section>
+    )
+}
+
 function JobSimilarityAnalysis({ job, loading }) {
     const { resumes, currentResume, setCurrentResume } = useResume();
     const { resumeScore, isComparing, messages } = useResumeAnalysis();
@@ -22,24 +56,7 @@ function JobSimilarityAnalysis({ job, loading }) {
                 
             </section>
 
-            <section id="resume-list">
-                <h3>Select Resume</h3>
-                <ol className="custom-ol">
-                    {resumes.map((resume, index) => (
-                        <li className={`custom-li ${currentResume._id === resume._id ? 'current': ''}`} key={resume._id}>
-                            <div className="wrapper">
-                                <h4>Resume {index + 1}</h4> 
-                                <i className="fa-solid fa-angle-down" aria-label="Toggle content visibility"></i>
-                            </div>
-                            {/* <span className="joined-skills">Matched skills: {getMatchedResumeSkills(resume)}</span> */}
-                        </li>
-                    ))}
-                </ol>
-                <button className="upload-resume-btn">
-                    <span>Or upload new resume</span>
-                    <i className="fa-solid fa-arrow-up"></i>
-                </button>
-            </section>
+            <ResumeList job={job} resumes={resumes} currentResume={currentResume} setCurrentResume={setCurrentResume}/>
         </section>
     )
 }

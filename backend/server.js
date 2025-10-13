f1d77adfde618325ae6ba90e7b5d821ba624ec09
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-13 08:00:53 +0800

    added message_attachments route in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use('/profile-pictures', express.static(path.join(__dirname, 'public', 'profile_pictures')))
-
 app.use('/company-logos', express.static(path.join(__dirname, 'public', 'company_logos')))
 app.use('/company_banners', express.static(path.join(__dirname, 'public', 'company_banners')))
 app.use('/company_images', express.static(path.join(__dirname, 'public', 'company_images')))
+app.use('/message-attachments', express.static(path.join(__dirname, 'public', 'message_attachments')))
 
 app.use('/api/users', userRoutes)
 app.use('/api/job-postings', jobPostingRoutes)

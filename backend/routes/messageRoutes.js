5585cb1e1a2c7703f8a558aaae1b178b554f3e2c
RoelVillaluz <roelo.villaluz@gmail.com>
2025-10-13 08:01:48 +0800

    updated createMessage route to handle upload to storage

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,5 +1,18 @@
 import express from 'express'
 import { getMessageById, getMessages, getMessagesByUser, createMessage, deleteMessage, updateMessage, markMessagesAsSeen } from '../controllers/messageController.js';
+import multer from "multer"
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'frontend/public/message_attachments')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+})
+
+const upload = multer({ storage: storage })
 
 const router = express.Router();
 
@@ -7,7 +20,7 @@ router.get('/', getMessages)
 router.get('/:messageId', getMessageById)
 router.get('/user/:userId', getMessagesByUser)
 
-router.post('/', createMessage)
+router.post('/', upload.single('attachment'), createMessage)
 
 router.patch('/mark-as-seen', markMessagesAsSeen)
 router.patch('/:messageId', updateMessage)

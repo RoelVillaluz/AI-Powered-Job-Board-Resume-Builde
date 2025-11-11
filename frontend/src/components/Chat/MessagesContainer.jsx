eb00de36f5fc6108fef4d2acc8286f9484f97d96
RoelVillaluz <roelo.villaluz@gmail.com>
2025-11-11 09:09:32 +0800

    added Virtuoso to MessagesContainer for improved performance

--- frontend/src/components/Chat/MessagesContainer.jsx
+++ frontend/src/components/Chat/MessagesContainer.jsx
@@ -1,26 +1,32 @@
 import { useData } from "../../contexts/DataProvider";
-import { useChatContext } from "../../contexts/chats/ChatContext";
 import { ReadReceiptsProvider } from "../../contexts/ReadReciptsContext";
-import useMessageOperations from "../../hooks/chats/useMessageOperations"
 import MessageGroup from "./MessageGroup";
+import { Virtuoso } from "react-virtuoso";
+import { useMessageOperationsContext } from "../../contexts/chats/MessageOperationsContext";
 
 function MessagesContainer({ showComposeMessage, messages }) {
     const { baseUrl } = useData();
+    const { loadOlderMessages } = useMessageOperationsContext();
 
     return (
         <ReadReceiptsProvider>
-            <div className="messages-container">
-                {!showComposeMessage && (
-                    <ul>
-                        {messages.length > 0 && (
-                            messages.map((group, groupIndex) => (
-                                <MessageGroup 
-                                    group={group} 
-                                    key={groupIndex}
-                                />
-                            ))
-                        )}
-                    </ul>
+            <div className="messages-container" style={{ height: '100%' }}>
+                {!showComposeMessage && messages.length > 0  && (
+                    <Virtuoso
+                        data={messages}
+                        firstItemIndex={0} // for prepending older messages
+                        initialTopMostItemIndex={messages.length - 1} // scroll to bottom (last message)
+                        itemContent={(index) => {
+                            const group = messages[index];
+                            if (!group) return null;
+                            return <MessageGroup key={group._id || index} group={group}/>
+                        }}
+                        atTopStateChange={(atTop) => {
+                            if (atTop) loadOlderMessages?.();
+                        }}
+                        followOutput="auto"
+                        style={{ height: '100%' }}
+                    />
                 )}
             </div>
         </ReadReceiptsProvider>

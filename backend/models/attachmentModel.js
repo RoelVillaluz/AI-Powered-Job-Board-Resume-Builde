7c2d11081f486219a63c21e996ad6417aa2b60c9
RoelVillaluz <roelo.villaluz@gmail.com>
2025-11-15 08:25:33 +0800

    created new Attachment model

---
+++ backend/models/attachmentModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const attachmentSchema = new mongoose.Schema({
+    conversation: { 
+        type: ObjectId, 
+        ref: 'Conversation', 
+        index: true 
+    },
+    message: { 
+        type: ObjectId, 
+        ref: 'Message', 
+        index: true 
+    },
+    fileName: { 
+        type: String, 
+        required: true 
+    },
+    fileSize: { 
+        type: Number, 
+        required: true 
+    }, // bytes
+    url: { 
+        type: String, 
+        required: true 
+    },
+    thumbnail: { 
+        type: String, 
+        default: null 
+    },
+    type: { 
+        type: String, 
+        enum: ['image', 'pdf', 'voice', 'video'], 
+        default: 'image' 
+    }
+}, { timestamps: true });
+
+const Attachment = mongoose.model("Attachment", attachmentSchema);
+export default Attachment;

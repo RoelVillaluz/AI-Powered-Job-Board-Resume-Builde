dab3758511457be82a58ccac5692363dfd83071e
RoelVillaluz <roelo.villaluz@gmail.com>
2025-11-21 10:17:37 +0800

    added function renderResourceItem to chatResources for dynamically rendering resouce list items

--- frontend/src/components/Chat/ChatResources.jsx
+++ frontend/src/components/Chat/ChatResources.jsx
@@ -11,6 +11,34 @@ function ChatResources({ currentConversation }) {
     const { baseUrl } = useData();
     const { resources, fetchResourceType } = useChatResources(currentConversation);
     const [currentResource, setCurrentResource] = useState(null);
+    const renderResourceItem = (icon, label, resourceKey, endPoint) => {
+        const resource = resources[resourceKey] || {
+            count: 0,
+            loading: false,
+            error: null
+        }
+
+        return (
+            <li className="resource-item">
+                <i className={`fa-solid fa-${icon}`}></i>
+                <div>
+                    <strong>{label}</strong>
+                    {resource.loading ? (
+                        <div className="skeleton text" style={{ 
+                            height: '1.25rem', 
+                            width: '10rem', 
+                            marginTop: '0.5rem' 
+                        }}></div>
+                        ) : resource.error ? (
+                            <p className="error-text">Error loading</p>
+                        ) : (
+                            <p>{resource.count} Item{resource.count !== 1 ? 's' : ''}</p>
+                        )}
+                </div>
+                <i className="fa-solid fa-angle-right"></i>
+            </li>
+        )
+    }
 
     return (
         <section className="chat-resources">

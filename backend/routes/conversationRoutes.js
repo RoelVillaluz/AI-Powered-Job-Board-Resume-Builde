95a967ee6ee9ccb0f54f10a92ab774e33cf99e0c
RoelVillaluz <roelo.villaluz@gmail.com>
2025-11-30 10:10:20 +0800

    removed trailing '/' from resource links route

--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -12,7 +12,7 @@ router.get('/:conversationId/resources/attachments/count', getAttachmentCountsBy
 router.get('/:conversationId/resources/pinned-messages', getPinnedMessagesByConversationId)
 router.get('/:conversationId/resources/pinned-messages/count', getPinnedMessagesCountByConversationId)
 
-router.get('/:conversationId/resources/links/', getLinksByConversationId)
+router.get('/:conversationId/resources/links', getLinksByConversationId)
 router.get('/:conversationId/resources/links/count', getLinkCountsByConversationId)
 
 router.get('/user/:userId', getConversationsByUser)

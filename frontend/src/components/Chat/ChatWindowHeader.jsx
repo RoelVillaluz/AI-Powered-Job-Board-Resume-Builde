3106c74f88e9f26511e149d6f2a04ed8e2b61ec3
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-12 09:49:11 +0800

    added first name and last name to chat window hgeader

--- frontend/src/components/Chat/ChatWindowHeader.jsx
+++ frontend/src/components/Chat/ChatWindowHeader.jsx
@@ -20,13 +20,15 @@ function ChatWindowHeader({ user, currentConversation, showComposeMessage, curre
         setSearchReceiverQuery('')
     }
 
+    console.log('Current Receiver: ', currentReceiver)
+
     if (currentConversation && !showComposeMessage) {
         return (
              <header>
                 <div className="user">
                     <img src={currentConversation.receiver.profilePicture} alt="" />
                     <address>
-                        <strong>{currentConversation.receiver.name}</strong>
+                        <strong>{currentConversation.receiver.firstName} {currentConversation.receiver.lastName}</strong>
                         <span>{currentConversation.receiver.email}</span>
                     </address>
                 </div>
@@ -47,8 +49,8 @@ function ChatWindowHeader({ user, currentConversation, showComposeMessage, curre
                         <input type="text" placeholder="Search for a name" value={searchReceiverQuery} onChange={(e) => setSearchReceiverQuery(e.target.value)}/>
                     ) : (
                         <div className="selected-receiver">
-                            <img src={currentReceiver?.profilePicture} alt={`${currentReceiver?.name}'s profile picture`} />
-                            <strong>{currentReceiver?.name} {currentReceiver?.name}</strong>
+                            <img src={currentReceiver?.profilePicture} alt={`${currentConversation.receiver.firstName} ${currentConversation.receiver.lastName}'s profile picture`} />
+                            <strong>{currentConversation.receiver.firstName} {currentConversation.receiver.lastName}</strong>
                             <button className="remove-receiver-btn" onClick={() => handleRemoveReceiver()}>
                                 <i className="fa-solid fa-xmark"></i>
                             </button>
@@ -60,8 +62,8 @@ function ChatWindowHeader({ user, currentConversation, showComposeMessage, curre
                             {searchReceiverResults.filter(r => r._id !== user._id).map((result, index) => (
                                 <li key={index}>
                                     <button onClick={() => handleReceiverSelect(result)}>
-                                        <img src={result.profilePicture} alt={`${result.name}'s profile picture`} />
-                                        <strong>{result.name}</strong>
+                                        <img src={result.profilePicture} alt={`${result.firstName} ${result.lastName}'s profile picture`} />
+                                        <strong>{result.firstName} {result.lastName}</strong>
                                     </button>
                                 </li>
                             ))}

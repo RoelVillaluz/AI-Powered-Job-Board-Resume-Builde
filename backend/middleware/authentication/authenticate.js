1aa2b703b864dff1d06eb7e7da730c9453c0ab67
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-14 09:00:18 +0800

    created authentication middleware that checks if user is logged in

---
+++ backend/middleware/authentication/authenticate.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { UnauthorizedError } from '../errorHandler.js';
+import { catchAsync } from '../../utils/errorUtils.js';
+
+export const authenticate = catchAsync(async (req, res, next) => {
+    // Get token from header
+    const token = req.headers.authorization?.replace('Bearer ', '')
+
+    if (!token) {
+        throw new UnauthorizedError('No authentication token provided.')
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        throw new UnauthorizedError('Invalid or expired token');
+    }
+})

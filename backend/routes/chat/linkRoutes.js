1c5eb303e6598b765e2651958e0a3394fd87e9c5
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-18 08:38:00 +0800

    added middleware to link routes

--- backend/routes/chat/linkRoutes.js
+++ backend/routes/chat/linkRoutes.js
@@ -1,9 +1,24 @@
 import express from "express"
 import { getLinkCountsByConversationId, getLinksByConversationId } from "../../controllers/chat/linksController.js"
+import { authenticate } from "../../middleware/authentication/authenticate.js";
+import { conversationIdSchema } from "../../validators/conversationValidators.js";
+import { authorizeConversation } from "../../middleware/authorization/conversation.js";
+import { validate } from "../../middleware/validation.js";
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/', getLinksByConversationId)
-router.get('/count', getLinkCountsByConversationId)
+router.get('/', 
+    authenticate,
+    validate(conversationIdSchema, 'params'),
+    authorizeConversation,
+    getLinksByConversationId
+)
+
+router.get('/count', 
+    authenticate,
+    validate(conversationIdSchema, 'params'),
+    authorizeConversation,
+    getLinkCountsByConversationId
+)
 
 export default router

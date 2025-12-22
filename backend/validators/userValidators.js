49e1d9d4c869e86d1f6131d74e186213056a2577
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-22 10:17:36 +0800

    created loginSchema validator

--- backend/validators/userValidators.js
+++ backend/validators/userValidators.js
@@ -37,3 +37,17 @@ export const registerUserSchema = Joi.object({
         }),
 })
 
+export const loginSchema = Joi.object({
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.pattern.base': 'Invalid email format',
+            'string.empty': 'Email is required'
+        }),
+    password: Joi.string()
+        .required()
+        .messages({
+            'string.empty': 'Password is required'
+        })
+})

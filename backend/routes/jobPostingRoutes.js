5f2a2f85f12a0f920d2c42e7ef353e4689b14341
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-22 13:08:03 +0800

    added schema validation for jobPosting POST route

--- backend/routes/jobPostingRoutes.js
+++ backend/routes/jobPostingRoutes.js
@@ -1,4 +1,6 @@
 import express from "express"
+import { validate } from "../middleware/validation.js"
+import { createJobPostingSchema } from "../validators/jobPostingValidators.js"
 import { getJobPostings, getJobPosting, createJobPosting, updateJobPosting, deleteJobPosting } from "../controllers/jobPostingController.js"
 
 const router = express.Router()
@@ -6,7 +8,10 @@ const router = express.Router()
 router.get('/', getJobPostings)
 router.get('/:id', getJobPosting)
 
-router.post('/', createJobPosting)
+router.post('/', 
+    validate(createJobPostingSchema, 'body'), // Validate job posting format
+    createJobPosting
+)
 
 router.patch('/:id', updateJobPosting)
 router.delete('/:id', deleteJobPosting)

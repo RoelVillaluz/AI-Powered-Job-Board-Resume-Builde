9a5ea4ddbe9c0e22751bfbc7f2fb2ec9406a14c6
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-22 10:18:18 +0800

    fixed magic numbers in conversationValidators

--- backend/validators/conversationValidators.js
+++ backend/validators/conversationValidators.js
@@ -1,8 +1,11 @@
 import Joi from "joi";
 
+const LIMIT_DEFAULT = 10;
+const LIMIT_MAX = 100
+
 export const paginationSchema = Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    limit: Joi.number().integer().min(1).max(100).default(10)
+    limit: Joi.number().integer().min(1).max(LIMIT_MAX).default(LIMIT_DEFAULT)
 });
 
 export const conversationIdSchema = Joi.object({

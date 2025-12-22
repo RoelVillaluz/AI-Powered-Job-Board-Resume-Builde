88494704930126e2a52c88dd8d795980be3f7a58
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-22 10:17:20 +0800

    created registerUserSchema validator

--- backend/validators/userValidators.js
+++ backend/validators/userValidators.js
@@ -8,3 +8,32 @@ export const userIdSchema = Joi.object({
         'string.pattern.base': 'Invalid user ID format'
         })
 })
+
+export const registerUserSchema = Joi.object({
+    firstName: Joi.string()
+        .min(2)
+        .max(50)
+        .required()
+        .messages({
+            'string.empty': 'First name is required',
+            'string.min': 'First name must be at least 2 characters',
+            'string.max': 'First name cannot exceed 50 characters'
+        }),
+    lastName: Joi.string()
+        .min(2)
+        .max(50)
+        .required()
+        .messages({
+            'string.empty': 'Last name is required',
+            'string.min': 'Last name must be at least 2 characters',
+            'string.max': 'Last name cannot exceed 50 characters'
+        }),
+    email: Joi.string()
+        .email()
+        .required()
+        .messages({
+            'string.empty': 'Email is rquired',
+            'string.pattern.base': 'Invalid email format'
+        }),
+})
+

11db3fce37edd801aa4cc1e47bf5256a1392dbd7
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-23 09:26:50 +0800

    added authentication to post, patch, and delete job posting routes

--- backend/routes/jobPostingRoutes.js
+++ backend/routes/jobPostingRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import { validate } from "../middleware/validation.js"
 import { createJobPostingSchema } from "../validators/jobPostingValidators.js"
 import { getJobPostings, getJobPosting, createJobPosting, updateJobPosting, deleteJobPosting } from "../controllers/jobPostingController.js"
+import { authenticate } from "../middleware/authentication/authenticate.js"
 
 const router = express.Router()
 
@@ -9,11 +10,19 @@ router.get('/', getJobPostings)
 router.get('/:id', getJobPosting)
 
 router.post('/', 
+    authenticate,
     validate(createJobPostingSchema, 'body'), // Validate job posting format
     createJobPosting
 )
 
-router.patch('/:id', updateJobPosting)
-router.delete('/:id', deleteJobPosting)
+router.patch('/:id', 
+    authenticate,
+    updateJobPosting
+)
+
+router.delete('/:id', 
+    authenticate,
+    deleteJobPosting
+)
 
 export default router

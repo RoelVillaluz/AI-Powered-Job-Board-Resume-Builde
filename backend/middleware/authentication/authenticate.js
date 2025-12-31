9a33f82a94ada730b0b8c7e33cf6096b8376c0d4
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-31 11:28:50 +0800

    refactor(authenticate): removed redundant try-catch block in catchAsync function

--- backend/middleware/authentication/authenticate.js
+++ backend/middleware/authentication/authenticate.js
@@ -1,20 +1,22 @@
 import jwt from 'jsonwebtoken';
-import { UnauthorizedError } from '../errorHandler.js';
+import { NotFoundError, UnauthorizedError } from '../errorHandler.js';
 import { catchAsync } from '../../utils/errorUtils.js';
+import User from '../../models/userModel.js';
 
 export const authenticate = catchAsync(async (req, res, next) => {
-    // Get token from header
     const token = req.headers.authorization?.replace('Bearer ', '')
-
+    
     if (!token) {
         throw new UnauthorizedError('No authentication token provided.')
     }
+    
+    // Let catchAsync handle jwt.verify errors
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        throw new UnauthorizedError('Invalid or expired token');
-    }
+    // Verify if user exists in the database
+    const user = await User.findById(decoded._id).select('-password')
+    if (!user) throw new NotFoundError('User')
+
+    req.user = decoded;
+    next();
 })

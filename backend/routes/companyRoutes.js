3f5b2e01ca34cb382b710a118065fd917ae2c098
RoelVillaluz <roelo.villaluz@gmail.com>
2025-12-31 15:03:41 +0800

    feat(company): add authorization and ownership middleware to create company route

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,12 +1,24 @@
 import express from "express"
 import { createCompany, deleteCompany, getCompanies, getCompany, updateCompany } from "../controllers/companyController.js";
+import { authenticate } from "../middleware/authentication/authenticate.js"
+import { requireEmployerRole, enforceCompanyOwnership, ensureSingleCompanyPerEmployer } from "../middleware/authorization/companyAuthorization.js";
+import { validate } from "../middleware/validation.js";
+import { createCompanySchema } from "../validators/companyValidator.js";
 
 const router = express.Router();
 
 router.get('/', getCompanies)
 router.get('/:id', getCompany)
 
-router.post('/', createCompany)
+router.post('/', 
+    authenticate,                // checks if user is logged in
+    requireEmployerRole,         // only employers
+    enforceCompanyOwnership,     // prevent creating for another user
+    ensureSingleCompanyPerEmployer, // prevents user from having more than one company
+    validate(createCompanySchema, 'body'), // body validation without user field
+    createCompany
+)
+
 router.patch('/:id', updateCompany)
 
 router.delete('/:id', deleteCompany)

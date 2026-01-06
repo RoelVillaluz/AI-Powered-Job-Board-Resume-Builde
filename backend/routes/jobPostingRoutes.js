0c3f8c9e731b806f6fb823914f1498954c0ffd14
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-06 09:16:25 +0800

    refactor: added dynamic require-role middleware to POST job route

--- backend/routes/jobPostingRoutes.js
+++ backend/routes/jobPostingRoutes.js
@@ -3,7 +3,7 @@ import { validate } from "../middleware/validation.js"
 import { createJobPostingSchema } from "../validators/jobPostingValidators.js"
 import { getJobPostings, getJobPosting, createJobPosting, updateJobPosting, deleteJobPosting } from "../controllers/jobPostingController.js"
 import { authenticate } from "../middleware/authentication/authenticate.js"
-import { requireEmployerRole } from "../middleware/authorization/companyAuthorization.js"
+import { requireRole } from "../middleware/authorization/roleAuthorization.js"
 
 const router = express.Router()
 
@@ -12,7 +12,7 @@ router.get('/:id', getJobPosting)
 
 router.post('/', 
     authenticate,                             // 1. Check if user is logged in
-    requireEmployerRole,                      // 2. Check if user is employer
+    requireRole('employer'),                  // 2. Check if user is employer
     validate(createJobPostingSchema, 'body'), // 3. Validate job posting format
     createJobPosting
 )

145ab40b2eb2d253fc0f2916961dd2f83b590031
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-06 09:15:00 +0800

    feat(auth-routes): added change-password route with middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { resendVerificationCode, verifyUser, loginUser } from "../controllers/auth/authControllers.js"
-import { attachVerificationUser, checkEmailIfExists, validateVerificationCode } from "../middleware/authorization/userAuthorization.js"
+import { resendVerificationCode, verifyUser, loginUser, changePassword } from "../controllers/auth/authControllers.js"
+import { attachVerificationUser, validateVerificationCode } from "../middleware/authorization/userAuthorization.js"
+import { checkEmailIfExists } from "../middleware/resourceCheck/emails.js"
 import { verificationSchema } from "../validators/verificationCodeValidator.js"
 import { validate } from "../middleware/validation.js"
+import { passwordSchema } from "../validators/userValidators.js"
 
 const router = express.Router()
 
@@ -21,6 +23,12 @@ router.post('/verify',
 router.post('/login', 
     checkEmailIfExists,
     loginUser
+)
+
+router.put('/change-password', 
+    validate(passwordSchema, 'body'),
+    checkEmailIfExists,
+    changePassword
 )
 
 export default router

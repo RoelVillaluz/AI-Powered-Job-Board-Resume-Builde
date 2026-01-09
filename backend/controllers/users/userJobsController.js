76d53338ad32f31fac962658dd27e776785f5f7d
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-09 08:38:29 +0800

    fix(user-jobs): imported correct directories for user repositories

--- backend/controllers/users/userJobsController.js
+++ backend/controllers/users/userJobsController.js
@@ -1,5 +1,6 @@
 import { sendResponse, STATUS_MESSAGES } from "../../constants.js";
-import * as UserGetRepo from '../../repositories/users/userGetRepos.js';
+import * as UserGetRepo from "../../repositories/users/userGetRepos.js";
+import * as UserSetRepo from "../../repositories/users/userSetRepos.js";
 import { catchAsync } from "../../utils/errorUtils.js";
 
 export const getUserInteractedJobs = catchAsync(async (req, res) => {
@@ -14,7 +15,7 @@ export const toggleSaveJob = catchAsync(async (req, res) => {
     const { jobId } = req.params;
     const userId = req.user._id || req.user.id
 
-    const data = await UserGetRepo.toggleSaveJob(jobId, userId)
+    const data = await UserSetRepo.toggleSaveJob(jobId, userId)
 
     return sendResponse(res, { ...STATUS_MESSAGES.SUCCESS.FETCH, data })
 })

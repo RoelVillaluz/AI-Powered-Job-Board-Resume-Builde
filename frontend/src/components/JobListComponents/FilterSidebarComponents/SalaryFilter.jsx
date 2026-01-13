13974cede31572ba01ccc9dbebd45439dff4cc0b
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-13 09:42:50 +0800

    fix: added missing return statement in SalaryFilter component

--- frontend/src/components/JobListComponents/FilterSidebarComponents/SalaryFilter.jsx
+++ frontend/src/components/JobListComponents/FilterSidebarComponents/SalaryFilter.jsx
@@ -1,29 +1,31 @@
 const SalaryFilter = ({ min, max, onChange }) => {
-    <div>
-        <h4>Salary Range</h4>
-        <div className="min-max-container">
-            <div>
-                <label htmlFor="min-salary">MIN</label>
-                <input 
-                    type="number" 
-                    id="min-salary"
-                    value={min ?? ''} 
-                    onChange={(e) => onChange("salary", e.target.value, "min")}
-                    placeholder="0"
-                />
-            </div>
-            <div>
-                <label htmlFor="max-salary">MAX</label>
-                <input 
-                    type="number" 
-                    id="max-salary"
-                    value={max ?? ''}
-                    onChange={(e) => onChange("salary", e.target.value, "max")}
-                    placeholder="200000"
-                />
+    return (
+        <div>
+            <h4>Salary Range</h4>
+            <div className="min-max-container">
+                <div>
+                    <label htmlFor="min-salary">MIN</label>
+                    <input 
+                        type="number" 
+                        id="min-salary"
+                        value={min ?? ''} 
+                        onChange={(e) => onChange(e.target.value, "min")}
+                        placeholder="0"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="max-salary">MAX</label>
+                    <input 
+                        type="number" 
+                        id="max-salary"
+                        value={max ?? ''}
+                        onChange={(e) => onChange(e.target.value, "max")}
+                        placeholder="200000"
+                    />
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 export default SalaryFilter

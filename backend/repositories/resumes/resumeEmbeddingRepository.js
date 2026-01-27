01b785aef92ad685452e608c16e6d04c362c52c5
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-27 09:51:21 +0800

    feat: created repositories for resume and job embeddings, resume score, resume job comparisons

---
+++ backend/repositories/resumes/resumeEmbeddingRepository.js
@@ -0,0 +1,23 @@
+import ResumeEmbedding from "../../models/resumes/resumeEmbeddingsModel.js"
+
+export const getResumeEmbeddingsRepo = async (resumeId) => {
+    return await ResumeEmbedding.findOne({
+        resume: resumeId
+    })
+    .select('-model');
+}
+
+export const createResumeEmbeddingRepo = async (embeddingData) => {
+    const newEmbedding = new ResumeEmbedding(embeddingData);
+    return await newEmbedding.save();
+}
+
+export const updateResumeEmbeddingRepo = async (resumeId, updateData) => {
+    const updatedEmbeddings = await ResumeEmbedding.findByIdAndUpdate(
+        resumeId,
+        updateData,
+        { new: true }
+    )
+
+    return updatedEmbeddings
+}
---
+++ backend/repositories/resumes/resumeScoreRepository.js
@@ -0,0 +1,30 @@
+import ResumeScore from "../../models/resumes/resumeScoreModel.js"
+
+export const getResumeScoreRepo = async (resumeId) => {
+    return await ResumeScore.findOne({
+        resume: resumeId
+    })
+    .select('-calculationVersion predictedSalary')
+}
+
+export const getPredictedSalaryRepo = async (resumeId) => {
+    return await ResumeScore.findOne({
+        resume: resumeId
+    })
+    .select('resume predictedSalary')
+}
+
+export const createResumeScoreRepo = async (scoreData) => {
+    const newScore = new ResumeScore(scoreData);
+    return await newScore.save();
+}
+
+export const updateResumeScoreRepo = async (resumeId, updateData) => {
+    const updatedScore = ResumeScore.findByIdAndUpdate(
+        resumeId,
+        updateData,
+        { new: true }
+    )
+
+    return updatedScore
+}

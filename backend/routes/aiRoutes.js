7de9f90a4ab0e3a168837ee6b0597a5c018cb609
RoelVillaluz <roelo.villaluz@gmail.com>
2026-01-30 08:58:42 +0800

    feat(ai-routes): removed resume score route

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,11 +1,10 @@
 import express from "express"
-import { getJobRecommendations, getRecommendedSkills, getResumeScore, getPredictedSalary, recommendCompanies, compareResumeAndJob } from "../controllers/aiController.js";
+import { getJobRecommendations, getRecommendedSkills, getPredictedSalary, recommendCompanies, compareResumeAndJob } from "../controllers/aiController.js";
 
 const router = express.Router();
 
 router.get('/job-recommendations/:userId', getJobRecommendations)
 router.get('/skill-recommendations/:userId', getRecommendedSkills)
-router.get('/resume-score/:resumeId', getResumeScore)
 router.get('/predicted-salary/:resumeId', getPredictedSalary)
 router.get('/recommend-companies/:userId', recommendCompanies)
 router.get('/compare/:resumeId/:jobId', compareResumeAndJob)

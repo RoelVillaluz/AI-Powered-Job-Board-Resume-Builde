9a88877c090845fef89b5b222c38aa6881f3207d
RoelVillaluz <roelo.villaluz@gmail.com>
2026-03-15 12:59:05 +0800

    feat: added 'admin' to user role enum

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         default: undefined,
-        enum: ['jobseeker', 'employer'],
+        enum: ['jobseeker', 'employer', 'admin'],
     },
     profilePicture: {
         type: String,

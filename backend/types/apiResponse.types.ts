55742dcccf7dd6826427a966834dfcc746b0e179
RoelVillaluz <roelo.villaluz@gmail.com>
2026-03-17 19:47:35 +0800

    feat: created ApiQueueResponse type for queue respnose data

--- backend/types/apiResponse.types.ts
+++ backend/types/apiResponse.types.ts
@@ -1,6 +1,14 @@
 // Generic API response type
-export interface ApiResponse<T = any> {
+export interface ApiSendResponse<T = any> {
     success: boolean;            // true for success, false for error
     formattedMessage: string;    // human-readable message
     data?: T;                    // optional payload
+}
+
+export interface ApiQueueResponse<T = any> {
+    success: boolean;
+    cached: boolean;
+    message: string;
+    jobId: string;
+    statusUrl: string;
 }

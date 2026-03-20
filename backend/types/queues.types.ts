9b27635526ac0833e31c4b1de09b75c63b55bcb4
RoelVillaluz <roelo.villaluz@gmail.com>
2026-03-20 15:33:45 +0800

    feat: created QueueResult type

--- backend/types/queues.types.ts
+++ backend/types/queues.types.ts
@@ -1,3 +1,7 @@
 export interface QueueJob {
     updateProgress: (percent: number) => Promise<void> | void;
 }
+
+export type QueueResult<T> =
+    | { type: 'queued'; jobId: string }
+    | { type: 'executed'; data: T };

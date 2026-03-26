fc021ca7e31cc55c8014298147b73eb00e395df1
RoelVillaluz <roelo.villaluz@gmail.com>
2026-03-26 10:37:46 +0800

    feat(create job form): created initial form data

--- frontend/constants/formSchemas.ts
+++ frontend/constants/formSchemas.ts
@@ -1,3 +1,4 @@
+import type { CreateJobFormData } from "../types/forms/createJobForm.types";
 import type { JobseekerFormData, EmployerFormData } from "../types/forms/getStartedForm.types";
 
 export const JOBSEEKER_INITIAL_FORM_DATA: Omit<JobseekerFormData, 'user'> = {
@@ -26,4 +27,28 @@ export const COMPANY_INITIAL_FORM_DATA: Omit<EmployerFormData, 'user'> = {
     size: "",
     description: "",
     logo: "",
+};
+
+export const CREATE_JOB_INITIAL_FORM_DATA: CreateJobFormData = {
+    title: "",
+    location: "",
+    jobType: "Full-Time",
+    company: '',
+    experienceLevel: undefined,
+    salary: {
+        currency: "$",
+        salary: {
+            min: null,
+            max: null,
+        },
+        frequency: "year",
+    },
+    requirements: {
+        description: "",
+        education: undefined,
+        yearsOfExperience: undefined,
+        certifications: [],
+    },
+    skills: [],
+    preScreeningQuestions: [],
 };

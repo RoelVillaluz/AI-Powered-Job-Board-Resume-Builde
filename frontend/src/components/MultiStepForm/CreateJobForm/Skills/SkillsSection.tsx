c48a4ca9e55e723a440f671a58545f58cb35cd64
RoelVillaluz <roelo.villaluz@gmail.com>
2026-04-07 05:46:48 +0800

    feat: created suggsted skills modal for easy skill selection in skills section

--- frontend/src/components/MultiStepForm/CreateJobForm/Skills/SkillsSection.tsx
+++ frontend/src/components/MultiStepForm/CreateJobForm/Skills/SkillsSection.tsx
@@ -1,6 +1,21 @@
 import { SkillsFormGroup } from "./SkillsFormGroup";
 import { RequirementsFormGroup } from "../Requirements/RequirementsFormGroup";
+import { SuggestedSkillsModal } from "./SuggestedSkillsModal";
+import { useState } from "react";
+import { useJobForm } from "../../../../contexts/JobFormContexts/JobPostingFormContext";
+import type { FormSkill } from "../../../../../types/forms/createJobForm.types";
 
+export type TopSkill = {
+  _id?: string | undefined;
+  skillName: string;
+    requirementLevel?: 'Required' | 'Preferred' | 'Nice-to-Have' | undefined;
+}
+
+const mapTopSkillToFormSkill = (skill: TopSkill): FormSkill => ({
+  _id: skill._id ?? "",
+  name: skill.skillName,
+  requirementLevel: skill.requirementLevel ?? 'Required' // default to Required
+});
 
 /**
  * SkillsSection
@@ -18,7 +33,41 @@ import { RequirementsFormGroup } from "../Requirements/RequirementsFormGroup";
  * `formData` is only written to on step 3, not on search or select.
  */
 export const SkillsSection = () => {
-  
+  const { formData, setFormData } = useJobForm();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
+
+  const addedSkillIds = new Set(
+    formData.skills
+      .map((s) => s._id)
+      .filter((id): id is string => !!id)
+  )
+
+  const handleCloseModal = () => {
+    setIsModalOpen((prev) => !prev)
+  }
+
+  const handleAddSkill = (skill: TopSkill) => {
+    setFormData((prev) => ({
+      ...prev,
+      skills: [...prev.skills, mapTopSkillToFormSkill(skill)],
+    }));
+  };
+
+  const handleRemoveSkill = (skill: TopSkill) => {
+    setFormData((prev) => ({
+      ...prev,
+      skills: prev.skills.filter((s) => s._id !== skill._id)
+    }))
+  }
+
+  const handleAddAllSkills = (allSkills: TopSkill[]) => {
+    const mappedSkills = allSkills.map(mapTopSkillToFormSkill);
+    setFormData((prev) => ({
+      ...prev,
+      skills: [...prev.skills, ...mappedSkills],
+    }));
+    handleCloseModal();
+  };
 
   // ─── Render ────────────────────────────────────────────────────────────────
 
@@ -30,6 +79,17 @@ export const SkillsSection = () => {
         </header>
 
         <SkillsFormGroup/>
+
+        {isModalOpen && (
+          <SuggestedSkillsModal
+            jobTitle={formData.title}
+            onClose={handleCloseModal}
+            onAdd={handleAddSkill}
+            onRemove={handleRemoveSkill}
+            onAddAll={handleAddAllSkills}
+            addedSkillIds={addedSkillIds}
+          />
+        )}
 
     </section>
   );

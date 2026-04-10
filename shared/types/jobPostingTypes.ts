db192dd78f807b4cb77a818002dca5b1c72d0079
RoelVillaluz <roelo.villaluz@gmail.com>
2026-04-10 09:58:05 +0800

    feat: created shared JobPosting type

---
+++ shared/types/jobPostingTypes.ts
@@ -0,0 +1,25 @@
+export type JobPosting = {
+  _id: string;
+  title: { _id: string; name: string };
+  company: { _id: string; name: string; industry: string[]; logo: string };
+  status: string;
+  location: { _id: string; name: string };
+  experienceLevel: string;
+  jobType: string;
+  postedAt: string;
+  preScreeningQuestions: any[];
+  requirements: {
+    description: string;
+    education: string;
+    yearsOfExperience: number;
+    certifications: string[];
+  };
+  salary: {
+    currency: string;
+    min: number;
+    max: number;
+    frequency: string;
+  };
+  skills: any[];
+  applicants: any[];
+};

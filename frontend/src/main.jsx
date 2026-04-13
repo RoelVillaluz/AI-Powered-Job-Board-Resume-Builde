739638cf309687f3d4e0cb7bcf2b23175edad07f
RoelVillaluz <roelo.villaluz@gmail.com>
2026-04-14 06:58:44 +0800

    feat: created ui.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/styles.css'
 import './styles/navbar.css'
+import './styles/ui.css'
 
 import './styles/forms/base.css'
 import './styles/forms/auth.css'

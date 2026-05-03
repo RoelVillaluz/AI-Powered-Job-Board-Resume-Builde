79d1fd275a3dc5913e1d97a763627fc6fc5cf1cb
Roel Villaluz <roelo.villaluz@gmail.com>
2026-05-03 18:38:02 +0800

    feat: registered workerREgistryV2 in generateEmbeddings processes

--- backend/src/infrastructure/jobs/processes/generateEmbeddings.ts
+++ backend/src/infrastructure/jobs/processes/generateEmbeddings.ts
@@ -1,15 +1,21 @@
 import { shutdownWorkers } from "../workers/workersRegistry.js";
-
+import { shutdownWorkersV2 } from "../workers/workerRegistryV2.js";
 import logger from "../../../utils/logger.js";
 
 logger.info("[EMBEDDING WORKERS] Initialized");
 
 process.on("SIGTERM", async () => {
-    await shutdownWorkers();
+    await Promise.all([
+        shutdownWorkers(),
+        shutdownWorkersV2(),
+    ]);
     process.exit(0);
 });
 
 process.on("SIGINT", async () => {
-    await shutdownWorkers();
+    await Promise.all([
+        shutdownWorkers(),
+        shutdownWorkersV2(),
+    ]);
     process.exit(0);
 });

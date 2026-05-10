b19f5c27b8912b8b12632b061167a7c19c2b9c1e
Roel Villaluz <roelo.villaluz@gmail.com>
2026-05-10 08:01:09 +0800

    feat(user profile section): added default profile picture and jobTitle

--- frontend/src/components/Dashboard/UserProfileSection.jsx
+++ frontend/src/components/Dashboard/UserProfileSection.jsx
@@ -1,8 +1,10 @@
+import { useResumeStore } from "../../stores/resumeStore";
 import { useAuthStore } from "../../stores/authStore";
 import { Link } from "react-router-dom"
 
 function UserProfileSection() {
     const user = useAuthStore(state => state.user);
+    const jobTitle = useResumeStore(state => state.currentResume?.jobTitle?.name);
     const isLoading = useAuthStore(state => state.isLoading);
 
     const fullName = `${user.firstName} ${user.lastName}`
@@ -12,10 +14,14 @@ function UserProfileSection() {
             {!isLoading && (
                 <>
                     <Link to={`profile/${user._id}`} className="image-container" aria-label={`Go to ${fullName}'s profile`}>
-                        <img src={user.profilePicture} alt={`${fullName}'s profile picture`} />
+                        {user.profilePicture ? (
+                            <img src={user.profilePicture} alt={`${fullName}'s profile picture`} />
+                        ) : (
+                            <img src={`public/profile_pictures/default.jpg`}/>
+                        )}
                         <div className="details">
                             <h2 className="name">{fullName}</h2>
-                            <h4 className="job-title">Full Stack Developer</h4>
+                            <h4 className="job-title">{jobTitle}</h4>
                             <p className="address"><i className="fa-solid fa-location-dot" aria-hidden="true"></i>Legazpi City, Philippines</p>
                         </div>
                     </Link>
